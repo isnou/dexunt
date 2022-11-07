2fbe6f925714571c46f06b55427b5d93efef217c
isnou <nouioua0islem@gmail.com>
2022-11-07 04:04:12 +0100

    ffs

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect
 
 
 def main_shop_home(request):
-    return render(request, "en/main-shop/home.html")
+    return render(request, "ltr/main-shop/home.html")
