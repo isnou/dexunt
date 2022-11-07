743bde0f755e9e4bd734810c5ff50e34221456a2
isnou <nouioua0islem@gmail.com>
2022-11-07 04:01:26 +0100

    ff

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.utils.translation import gettext as _
 
 
 def main_shop_home(request):
-    return render(request, "main-shop/home.html")
+    return render(request, "en/main-shop/home.html")
