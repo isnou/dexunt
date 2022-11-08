136554ae014317998d13162c432b357d1c69dcd8
isnou <nouioua0islem@gmail.com>
2022-11-08 14:15:30 +0100

    title

---
+++ src/main_shop/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Content
+
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('lang', 'title')
+
+
+admin.site.register(Content, ContentAdmin)
---
+++ src/main_shop/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Content(models.Model):
+    lang = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.lang
