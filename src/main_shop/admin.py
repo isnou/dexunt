1044006474961afc5f18857d3b6182d7b13bd300
isnou <nouioua0islem@gmail.com>
2022-11-08 15:47:39 +0100

    Update admin.py

--- src/main_shop/admin.py
+++ src/main_shop/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import Content
 
 
-class ContentAdmin(admin.ModelAdmin):
-    list_display = 'lang'
-
-
-admin.site.register(Content, ContentAdmin)
+admin.site.register(Content)
