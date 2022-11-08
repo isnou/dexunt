77e294420c76372934a8c6cd6a6f6448ca6160f9
isnou <nouioua0islem@gmail.com>
2022-11-08 14:55:04 +0100

    ff

--- src/main_shop/context_processors.py
+++ src/main_shop/context_processors.py
@@ -1,18 +1,13 @@
+from .models import Content
+
+
 def extras(request):
+    content = Content.objects.all()
+    en = content.filter(lang='EN')
+    fr = content.filter(lang='FR')
+    ar = content.filter(lang='AR')
     return {
-        'en.home_title': "Dexunt Home Page",
-        'en.home_title_info': "Online Shopping Gifts & Deco",
-        'en.track_my_order': "track my order",
-        'en.search_text': "search products here",
-        'en.all_category': "all",
-        'fr.home_title': "Page d'accueil de Dexunt",
-        'fr.home_title_info': "Shopping en ligne cadeaux et déco",
-        'fr.track_my_order': "suivre ma commande",
-        'fr.search_text': "rechercher des produits ici",
-        'fr.all_category': "tout",
-        'ar.home_title': "الصفحة الرئيسية لـ Dexunt",
-        'ar.home_title_info': "التسوق عبر الإنترنت هدايا وديكو",
-        'ar.track_my_order': "تابع طلبي",
-        'ar.search_text': "البحث عن المنتجات هنا",
-        'ar.all_category': "الكل",
+        'en': en,
+        'fr': fr,
+        'ar': ar,
     }
--- src/main_shop/models.py
+++ src/main_shop/models.py
@@ -4,7 +4,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Content(models.Model):
     lang = models.CharField(max_length=20, unique=True)
-    title = models.CharField(max_length=200)
+    home_page_title = models.CharField(max_length=200)
 
     def __str__(self):
         return self.lang
