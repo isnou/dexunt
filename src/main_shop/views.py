bf3853cd0ee4939259f088e5beea1d7566c944e4
isnou <nouioua0islem@gmail.com>
2022-11-08 15:53:54 +0100

    Update views.py

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
+from .models import Content
 
 
 def main_shop_home(request, lang):
+    content = Content.objects.all()
+    en = content.filter(lang='EN')
+    fr = content.filter(lang='FR')
+    ar = content.filter(lang='AR')
+
     if lang == "ar":
         url = "rtl/main-shop/home.html"
     else:
         url = "ltr/main-shop/home.html"
     context = {
         'lang': lang,
+        'en': en,
+        'fr': fr,
+        'ar': ar,
     }
 
     return render(request, url, context)
