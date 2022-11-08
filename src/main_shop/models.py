a60016b7139a456d9a8c77d3af75927406fc380d
isnou <nouioua0islem@gmail.com>
2022-11-08 14:48:07 +0100

    cc

--- src/main_shop/models.py
+++ src/main_shop/models.py
@@ -3,7 +3,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Content(models.Model):
-    lang = models.CharField(max_length=20)
+    lang = models.CharField(max_length=20, unique=True)
     title = models.CharField(max_length=200)
 
     def __str__(self):
