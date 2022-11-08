d89de223ded0d42557672816d4c319f65631227b
isnou <nouioua0islem@gmail.com>
2022-11-08 15:58:45 +0100

    tt

--- src/main_shop/context_processors.py
+++ src/main_shop/context_processors.py
@@ -3,9 +3,9 @@ from .models import Content
 
 def extras(request):
     content = Content.objects.all()
-    en = content.filter(lang='EN')
-    fr = content.filter(lang='FR')
-    ar = content.filter(lang='AR')
+    en = content.all().filter(lang='EN')
+    fr = content.all().filter(lang='FR')
+    ar = content.all().filter(lang='AR')
     return {
         'en': en,
         'fr': fr,
--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render, redirect
-from .models import Content
 
 
 def main_shop_home(request, lang):
-    content = Content.objects.all()
-    en = content.all().filter(lang='EN')
-    fr = content.all().filter(lang='FR')
-    ar = content.all().filter(lang='AR')
-
     if lang == "ar":
         url = "rtl/main-shop/home.html"
     else:
         url = "ltr/main-shop/home.html"
     context = {
         'lang': lang,
-        'en': en,
-        'fr': fr,
-        'ar': ar,
     }
 
     return render(request, url, context)
