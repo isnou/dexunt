1370b0a88731f7d39490b1b6a412dc2219ae3d89
isnou <nouioua0islem@gmail.com>
2022-11-08 04:22:00 +0100

    home_

--- src/main_shop/context_processors.py
+++ src/main_shop/context_processors.py
@@ -1,25 +1,25 @@
 def extras(request):
     en = {
-        'title': "Dexunt Home Page",
-        'title-info': "Online Shopping Gifts & Deco",
-        'track-my-order': "track my order",
-        'search-text': "search products here",
-        'all-category': "all",
+        'home_title': "Dexunt Home Page",
+        'home_title_info': "Online Shopping Gifts & Deco",
+        'track_my_order': "track my order",
+        'search_text': "search products here",
+        'all_category': "all",
     }
 
     fr = {
-        'title': "Page d'accueil de Dexunt",
-        'title-info': "Shopping en ligne cadeaux et déco",
-        'track-my-order': "suivre ma commande",
-        'search-text': "rechercher des produits ici",
-        'all-category': "tout",
+        'home_title': "Page d'accueil de Dexunt",
+        'home_title_info': "Shopping en ligne cadeaux et déco",
+        'track_my_order': "suivre ma commande",
+        'search_text': "rechercher des produits ici",
+        'all_category': "tout",
     }
 
     ar = {
-        'title': "الصفحة الرئيسية لـ Dexunt",
-        'title-info': "التسوق عبر الإنترنت هدايا وديكو",
-        'track-my-order': "تابع طلبي",
-        'search-text': "البحث عن المنتجات هنا",
-        'all-category': "الكل",
+        'home_title': "الصفحة الرئيسية لـ Dexunt",
+        'home_title_info': "التسوق عبر الإنترنت هدايا وديكو",
+        'track_my_order': "تابع طلبي",
+        'search_text': "البحث عن المنتجات هنا",
+        'all_category': "الكل",
     }
     return en, fr, ar
