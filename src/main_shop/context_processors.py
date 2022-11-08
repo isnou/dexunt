fbf894bf0a68ef228e35a3ebaf842f892877b1d3
isnou <nouioua0islem@gmail.com>
2022-11-08 16:02:07 +0100

    Update context_processors.py

--- src/main_shop/context_processors.py
+++ src/main_shop/context_processors.py
@@ -3,9 +3,9 @@ from .models import Content
 
 def extras(request):
     content = Content.objects.all()
-    en = content.all().filter(lang='EN')
-    fr = content.all().filter(lang='FR')
-    ar = content.all().filter(lang='AR')
+    en = content.get(lang='EN')
+    fr = content.get(lang='FR')
+    ar = content.get(lang='AR')
     return {
         'en': en,
         'fr': fr,
