10f72eaad42a3c8d53d8e4f45e5e35fa34a0a26f
isnou <nouioua0islem@gmail.com>
2022-11-08 15:09:45 +0100

    ff

--- src/main_shop/models.py
+++ src/main_shop/models.py
@@ -4,7 +4,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Content(models.Model):
     lang = models.CharField(max_length=20, unique=True)
-    home_page_title = models.CharField(max_length=200)
+    first_banner_title = models.CharField(max_length=200)
+    first_banner_title = models.CharField(max_length=200)
 
     def __str__(self):
         return self.lang
