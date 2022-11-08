87e91f8b9f36390568ac08fc2212f618fd1b6d6f
isnou <nouioua0islem@gmail.com>
2022-11-08 04:31:07 +0100

    Update views.py

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -6,4 +6,7 @@ def main_shop_home(request, lang):
         url = "rtl/main-shop/home.html"
     else:
         url = "ltr/main-shop/home.html"
-    return render(request, url, lang=lang)
+    context = {
+        'lang': lang,
+    }
+    return render(request, url, context)
