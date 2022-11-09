63f898990ea72d377d54bfb2bac99f134ffaa265
isnou <nouioua0islem@gmail.com>
2022-11-09 02:19:22 +0100

    dd

--- src/main_shop/context_processors.py
+++ src/main_shop/context_processors.py
@@ -2,10 +2,6 @@ from .models import Content
 
 
 def main_page_content(request):
-    content = Content.objects.all()
-    en = content.get(lang='EN')
-    fr = content.get(lang='FR')
-    ar = content.get(lang='AR')
     return {
         'en': en,
         'fr': fr,
