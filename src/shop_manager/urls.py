cc3755d1f6f02c5c7ff8c15b73e5e8ec4ae5b616
isnou <nouioua0islem@gmail.com>
2022-11-12 20:58:25 +0100

    cc

--- src/shop_manager/urls.py
+++ src/shop_manager/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:lang>/', views.manager_dashboard, name='manager-dashboard'),
+    path('<str:lang>/', views.manager_dashboard, name='dashboard'),
 ]
