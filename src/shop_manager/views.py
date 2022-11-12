ebb51d5d59756fb52b26510adfb0f05572987069
isnou <nouioua0islem@gmail.com>
2022-11-12 21:02:36 +0100

    ff

--- src/shop_manager/views.py
+++ src/shop_manager/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, redirect
 
 def manager_dashboard(request, lang):
     if lang == "ar":
-        url = "rtl/manager/base.html"
+        url = "rtl/shop-manager/base.html"
     else:
-        url = "ltr/manager/base.html"
+        url = "ltr/shop-manager/base.html"
     context = {
         'lang': lang,
     }
