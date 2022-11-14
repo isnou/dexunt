e3052227361f84cf27662f851acf9b120d14043b
isnou <nouioua0islem@gmail.com>
2022-11-14 21:13:49 +0100

    Update admin.py

--- src/shop_manager/admin.py
+++ src/shop_manager/admin.py
@@ -6,5 +6,9 @@ class InventoryProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'get_album', 'sku', 'quantity', 'buy_price', 'get_features')
 
 
+class ShopProductFeaturesAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'sku', 'get_products', 'tag', 'sel_price', 'discount_price', 'get_features')
+
+
 admin.site.register(InventoryProduct, InventoryProductAdmin)
-admin.site.register(ShopProductFeatures)
+admin.site.register(ShopProductFeatures, ShopProductFeaturesAdmin)
