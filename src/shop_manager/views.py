e49a37a17538ff3b5af20d503760f72cabd987f3
isnou <nouioua0islem@gmail.com>
2022-11-14 23:36:28 +0100

    submit

--- src/shop_manager/views.py
+++ src/shop_manager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import InventoryProduct
 
 
 def manager_dashboard(request, lang):
@@ -26,8 +27,21 @@ def inventory(request, lang):
 def add_product(request, lang):
     if lang == "ar":
         url = "rtl/shop-manager/add-product.html"
+    elif lang == 'save_general_product_information':
+        url = "rtl/shop-manager/inventory.html"
+        lang = "en"
+        if request.method == 'POST':
+            product_name = request.POST.get('product_name', False)
+            buy_price = request.POST.get('buy_price', False)
+            quantity = request.POST.get('quantity', False)
+            new_product = InventoryProduct(product_name=product_name,
+                                           buy_price=buy_price,
+                                           quantity=quantity,
+                                           )
+            new_product.save()
     else:
         url = "ltr/shop-manager/add-product.html"
+
     context = {
         'lang': lang,
     }
