1bc116d0d8b69ecac100a079b288bcfd44dcf7a8
isnou <nouioua0islem@gmail.com>
2022-11-14 21:23:55 +0100

    Update admin.py

--- src/shop_manager/admin.py
+++ src/shop_manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import InventoryProduct, ShopProduct
+from .models import ProductAlbum, InventoryProductFeatures, ShopProductFeatures, InventoryProduct, ShopProduct
 
 
 class InventoryProductAdmin(admin.ModelAdmin):
@@ -10,5 +10,8 @@ class ShopProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'sku', 'get_products', 'tag', 'sel_price', 'discount_price', 'get_features')
 
 
+admin.site.register(ProductAlbum)
+admin.site.register(InventoryProductFeatures)
+admin.site.register(ShopProductFeatures)
 admin.site.register(InventoryProduct, InventoryProductAdmin)
 admin.site.register(ShopProduct, ShopProductAdmin)
