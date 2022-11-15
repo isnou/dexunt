9e7a5461826f9e8e83d750183c501f14def9e406
isnou <nouioua0islem@gmail.com>
2022-11-15 01:27:39 +0100

    actions

---
+++ src/shop_manager/actions.py
@@ -0,0 +1,26 @@
+
+
+def add_product_actions(request, action):
+    if action == "ar":
+        url = "rtl/shop-manager/add-product.html"
+        lang = "ar"
+    elif action == 'en_save_general_product_information':
+        url = "ltr/shop-manager/inventory.html"
+        lang = "en"
+        if request.method == 'POST':
+            product_name = request.POST.get('product_name', False)
+            buy_price = request.POST.get('buy_price', False)
+            quantity = request.POST.get('quantity', False)
+            new_product = InventoryProduct(product_name=product_name,
+                                           buy_price=buy_price,
+                                           quantity=quantity,
+                                           )
+            new_product.save()
+    else:
+        url = "ltr/shop-manager/add-product.html"
+        lang = "en"
+    result = {
+        'url': url,
+        'lang': lang,
+    }
+    return result
--- src/shop_manager/views.py
+++ src/shop_manager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import InventoryProduct
+from .actions import *
 
 
 def manager_dashboard(request, lang):
@@ -33,29 +34,3 @@ def add_product(request, action):
         'lang': lang,
     }
     return render(request, url, context)
-
-
-def add_product_actions(request, action):
-    if action == "ar":
-        url = "rtl/shop-manager/add-product.html"
-        lang = "ar"
-    elif action == 'en_save_general_product_information':
-        url = "ltr/shop-manager/inventory.html"
-        lang = "en"
-        if request.method == 'POST':
-            product_name = request.POST.get('product_name', False)
-            buy_price = request.POST.get('buy_price', False)
-            quantity = request.POST.get('quantity', False)
-            new_product = InventoryProduct(product_name=product_name,
-                                           buy_price=buy_price,
-                                           quantity=quantity,
-                                           )
-            new_product.save()
-    else:
-        url = "ltr/shop-manager/add-product.html"
-        lang = "en"
-    result = {
-        'url': url,
-        'lang': lang,
-    }
-    return result
