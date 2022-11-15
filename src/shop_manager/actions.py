6ef74289f8332aa26b4ec6d69d99c938c0585003
isnou <nouioua0islem@gmail.com>
2022-11-16 00:41:01 +0100

    Update actions.py

--- src/shop_manager/actions.py
+++ src/shop_manager/actions.py
@@ -63,8 +63,8 @@ def edit(request, action, sku):
         product_to_edit.delete()
         if request.method == 'POST':
             product_name = request.POST.get('product_name', False)
-            buy_price = request.POST.get('buy_price', False)
-            if int(buy_price) > 0:
+            buy_price = int(request.POST.get('buy_price', False))
+            if buy_price > 0:
                 prog += 1
             quantity = int(request.POST.get('quantity', False))
             thumb = request.FILES.get('thumb', False)
@@ -74,7 +74,7 @@ def edit(request, action, sku):
             if upc != 'NOBARCODE':
                 new_product = InventoryProduct(product_name=product_name,
                                                upc=upc,
-                                               buy_price=int(buy_price),
+                                               buy_price=buy_price,
                                                quantity=quantity,
                                                thumb=thumb,
                                                )
