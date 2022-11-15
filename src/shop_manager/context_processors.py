5405828d445ea8415f7fc50501332508b58d9bf5
isnou <nouioua0islem@gmail.com>
2022-11-15 21:45:57 +0100

    d

--- src/shop_manager/context_processors.py
+++ src/shop_manager/context_processors.py
@@ -6,7 +6,9 @@ def shop_manager_content(request):
         products = InventoryProduct.objects.all()
     except InventoryProduct.DoesNotExist:
         raise Http404("No products")
+    inventory_product_count = products.count()
     return {
         'products': products,
+        'inventory_product_count': inventory_product_count,
         'fool': [0, 1, 2, 3, 4],
     }
