13a3dd14dcc16dc823f8f263ee9850270c532564
isnou <nouioua0islem@gmail.com>
2022-11-15 18:24:56 +0100

    Update models.py

--- src/shop_manager/models.py
+++ src/shop_manager/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class ProductAlbum(models.Model):
