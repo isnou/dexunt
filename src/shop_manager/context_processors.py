2ab4a35c486b8f8700aef7caeef2181afa4fef43
isnou <nouioua0islem@gmail.com>
2022-11-15 02:09:09 +0100

    p

---
+++ src/shop_manager/context_processors.py
@@ -0,0 +1,9 @@
+from .models import InventoryProduct
+
+
+def shop_manager_content(request):
+    try:
+        products = InventoryProduct.objects.all()
+    except InventoryProduct.DoesNotExist:
+        raise Http404("No products")
+    return products
