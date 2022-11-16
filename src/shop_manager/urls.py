d24ef9b0bec39041a7a1f0541bbef6a755c2f95e
isnou <nouioua0islem@gmail.com>
2022-11-16 05:49:05 +0100

    gg

--- src/shop_manager/urls.py
+++ src/shop_manager/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('<str:action>/', views.manager_dashboard, name='dashboard'),
     path('<str:action>/inventory/', views.inventory, name='inventory'),
     path('<str:action>/add-product/', views.add_product, name='add-product'),
-    path('<str:action>/<str:sku>/show-product/', views.show_product, name='show-product'),
+    path('<str:action>/<str:sku>/view-product/', views.view_product, name='view-product'),
     path('<str:action>/<str:sku>/edit-product/', views.edit_product, name='edit-product'),
     path('<str:action>/<str:sku>/delete-product/', views.delete_product, name='delete-product'),
 ]
