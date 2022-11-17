a6340d7febf02cfb58b22daca9da13e02fd5c5af
isnou <nouioua0islem@gmail.com>
2022-11-17 08:58:37 +0100

    d

--- src/shop_manager/actions.py
+++ src/shop_manager/actions.py
@@ -104,7 +104,8 @@ def view(request, action, sku):
                 new_features.type = 'weight'
                 new_features.value = weight
                 if product_to_view.features.all().filter(type='weight').exists():
-                    product_to_view.features.all().filter(type='weight').value = weight
+                    product_to_view.features.all().filter(type='weight').delete()
+                    product_to_view.features.add(new_features)
                 else:
                     new_features.save()
                     product_to_view.features.add(new_features)
