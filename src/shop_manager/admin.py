f166a034bb2ef8483b43d3dfecc77878382282ef
isnou <nouioua0islem@gmail.com>
2022-11-18 04:00:21 +0100

    ff

--- src/shop_manager/admin.py
+++ src/shop_manager/admin.py
@@ -10,8 +10,12 @@ class ShopProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'sku', 'get_products', 'tag', 'sel_price', 'discount_price', 'get_features')
 
 
+class InventoryProductFeaturesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'value')
+
+
 admin.site.register(ProductAlbum)
-admin.site.register(InventoryProductFeatures)
+admin.site.register(InventoryProductFeatures, InventoryProductFeaturesAdmin)
 admin.site.register(ShopProductFeatures)
 admin.site.register(InventoryProduct, InventoryProductAdmin)
 admin.site.register(ShopProduct, ShopProductAdmin)
