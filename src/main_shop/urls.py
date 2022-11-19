a75b5fdf3cf650d900530249f6c6bef607096a55
isnou <nouioua0islem@gmail.com>
2022-11-19 06:16:51 +0100

    new theme

--- src/main_shop/urls.py
+++ src/main_shop/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:lang>/home-page', views.main_shop_home, name='main-shop-home'),
+    path('<str:action>/home-page', views.main_shop_home, name='main-shop-home'),
 ]
--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 
 
-def main_shop_home(request, lang):
-    if lang == "ar":
-        url = "rtl/main-shop/home.html"
-    else:
-        url = "ltr/main-shop/home.html"
+def main_shop_home(request, action):
+    if not request.session.get('language', None):
+        request.session['language'] = 'en'
+        request.session['direction'] = 'ltr/'
+    lang = request.session.get('language')
+    direction = request.session.get('direction')
+    url = direction + "main-shop/base.html"
     context = {
         'lang': lang,
     }
