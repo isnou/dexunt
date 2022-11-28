bd7996709eeda447bccde17b1024ec331c84fec5
isnou <nouioua0islem@gmail.com>
2022-11-28 05:22:27 +0100

    Update urls.py

--- src/shop_manager/urls.py
+++ src/shop_manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('<str:action>/', views.manager_dashboard, name='dashboard'),
     path('<str:action>/<str:sku>/inventory/', views.inventory, name='inventory'),
-    path('<str:action>/<str:sku>/<int:identity>/inventory-product/', views.inventory_product, name='inventory-product'),
+    path('<str:action>/<str:sku>/<int:identity>/inventory-edit/', views.inventory_edit, name='inventory-edit'),
     path('<str:action>/<str:sku>/<int:identity>/e-shop/', views.e_shop, name='e-shop'),
 ]
