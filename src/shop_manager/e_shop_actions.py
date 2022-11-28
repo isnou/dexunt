ccc053ea04f57b6a9cd39f1cb023123658ff7f66
isnou <nouioua0islem@gmail.com>
2022-11-28 23:48:39 +0100

    Update e_shop_actions.py

--- src/shop_manager/e_shop_actions.py
+++ src/shop_manager/e_shop_actions.py
@@ -18,21 +18,47 @@ def edit_first_main_banner(request):
 
     if request.method == 'POST':
         en_first_title = request.POST.get('en_first_title', False)
+        if not en_first_title:
+            en_first_title = layout.en_first_title
         en_second_title = request.POST.get('en_second_title', False)
+        if not en_second_title:
+            en_second_title = layout.en_second_title
         en_third_title = request.POST.get('en_third_title', False)
+        if not en_third_title:
+            en_third_title = layout.en_third_title
         en_button = request.POST.get('en_button', False)
+        if not en_button:
+            en_button = layout.en_button
 
         fr_first_title = request.POST.get('fr_first_title', False)
+        if not fr_first_title:
+            fr_first_title = layout.fr_first_title
         fr_second_title = request.POST.get('fr_second_title', False)
+        if not fr_second_title:
+            fr_second_title = layout.fr_second_title
         fr_third_title = request.POST.get('fr_third_title', False)
+        if not fr_third_title:
+            fr_third_title = layout.fr_third_title
         fr_button = request.POST.get('fr_button', False)
+        if not fr_button:
+            fr_button = layout.fr_button
 
         ar_first_title = request.POST.get('ar_first_title', False)
+        if not ar_first_title:
+            ar_first_title = layout.ar_first_title
         ar_second_title = request.POST.get('ar_second_title', False)
+        if not ar_second_title:
+            ar_second_title = layout.ar_second_title
         ar_third_title = request.POST.get('ar_third_title', False)
+        if not ar_third_title:
+            ar_third_title = layout.ar_third_title
         ar_button = request.POST.get('ar_button', False)
+        if not ar_button:
+            ar_button = layout.ar_button
 
         link = request.POST.get('link', False)
+        if not link:
+            link = layout.link
 
         thumb = request.FILES.get('thumb', False)
         if not thumb:
