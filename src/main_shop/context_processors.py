1403271590c7c2ac7c25c254667712920c99f255
isnou <nouioua0islem@gmail.com>
2022-11-29 01:55:12 +0100

    d

--- src/main_shop/context_processors.py
+++ src/main_shop/context_processors.py
@@ -6,17 +6,25 @@ def main_shop_content(request):
         layouts = Layout.objects.all()
     except InventoryProduct.DoesNotExist:
         raise Http404("No products")
-
     if layouts.filter(type='timer_banner').exists():
         expiration_date = layouts.get(type='timer_banner')
     else:
         expiration_date = Layout()
-
     if layouts.filter(type='first_main_banner').exists():
         first_main_banner = layouts.get(type='first_main_banner')
     else:
         first_main_banner = Layout()
+    if layouts.filter(type='second_main_banner').exists():
+        second_main_banner = layouts.get(type='second_main_banner')
+    else:
+        second_main_banner = Layout()
+    if layouts.filter(type='third_main_banner').exists():
+        third_main_banner = layouts.get(type='third_main_banner')
+    else:
+        third_main_banner = Layout()
     return {
         'expiration_date': expiration_date,
         'first_main_banner': first_main_banner,
+        'second_main_banner': second_main_banner,
+        'third_main_banner': third_main_banner,
     }
