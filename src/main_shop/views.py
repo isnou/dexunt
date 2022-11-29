e22fd67f1c6cda3c0cc977b5169131164485c9f5
isnou <nouioua0islem@gmail.com>
2022-11-29 13:10:42 +0100

    Update views.py

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render, redirect
 def main_shop_home(request, action):
     if not request.session.get('language', None):
         request.session['language'] = 'en'
-        request.session['url'] = 'ltr/main-shop/base.html'
+        request.session['direction'] = 'ltr/'
     lang = request.session.get('language')
-    url = str(request.session.get('url'))
+    direction = request.session.get('direction')
+    url = direction + "main-shop/base.html"
     context = {
         'lang': lang,
     }
