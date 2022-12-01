1db2108faa5517e19928fe88202c9c2b1dfef376
isnou <nouioua0islem@gmail.com>
2022-12-01 11:12:55 +0100

    d

--- src/shop_manager/e_shop_actions.py
+++ src/shop_manager/e_shop_actions.py
@@ -266,21 +266,29 @@ def add_showcase(request):
     rank = Layout.objects.all().filter(type='showcase').count() + 1
 
     if request.method == 'POST':
-        layout.en_second_title = request.POST.get('selected_type', False)
+        en_second_title = request.POST.get('selected_type', False)
 
-        layout.en_first_title = request.POST.get('en_title', False)
-        layout.en_button = request.POST.get('en_button', False)
+        en_first_title = request.POST.get('en_title', False)
+        en_button = request.POST.get('en_button', False)
 
-        layout.fr_first_title = request.POST.get('fr_title', False)
-        layout.fr_button = request.POST.get('fr_button', False)
+        fr_first_title = request.POST.get('fr_title', False)
+        fr_button = request.POST.get('fr_button', False)
 
-        layout.ar_first_title = request.POST.get('ar_title', False)
-        layout.ar_button = request.POST.get('ar_button', False)
+        ar_first_title = request.POST.get('ar_title', False)
+        ar_button = request.POST.get('ar_button', False)
 
-        layout.link = request.POST.get('link', False)
-        layout.type = 'showcase'
-        layout.rank = rank
-
+        link = request.POST.get('link', False)
+        layout = Layout(en_second_title=en_second_title,
+                        en_first_title=en_first_title,
+                        en_button=en_button,
+                        fr_first_title=fr_first_title,
+                        fr_button=fr_button,
+                        ar_first_title=ar_first_title,
+                        ar_button=ar_button,
+                        link=link,
+                        type='showcase',
+                        rank=rank,
+                        )
         layout.save()
 
     return {
