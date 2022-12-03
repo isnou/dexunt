dbc193c3fd8182c8310f0660215e794bdc872858
isnou <nouioua0islem@gmail.com>
2022-12-03 02:54:58 +0100

    s

--- src/shop_manager/e_shop_actions.py
+++ src/shop_manager/e_shop_actions.py
@@ -168,32 +168,32 @@ def add_movable_banner(request):
 
     if request.method == 'POST':
 
-        en_third_title = request.POST.get('selected_type', False)
+        en_second_title = request.POST.get('selected_type', False)
 
         en_first_title = request.POST.get('en_title', False)
-        en_second_title = request.POST.get('en_message', False)
+        en_third_title = request.POST.get('en_message', False)
         en_button = request.POST.get('en_button', False)
 
         fr_first_title = request.POST.get('fr_title', False)
-        fr_second_title = request.POST.get('fr_message', False)
+        fr_third_title = request.POST.get('fr_message', False)
         fr_button = request.POST.get('fr_button', False)
 
         ar_first_title = request.POST.get('ar_title', False)
-        ar_second_title = request.POST.get('ar_message', False)
+        ar_third_title = request.POST.get('ar_message', False)
         ar_button = request.POST.get('ar_button', False)
 
         thumb = request.FILES.get('thumb', False)
 
         link = request.POST.get('link', False)
-        layout = Layout(en_third_title=en_third_title,
+        layout = Layout(en_second_title=en_second_title,
                         en_first_title=en_first_title,
-                        en_second_title=en_second_title,
+                        en_third_title=en_third_title,
                         en_button=en_button,
                         fr_first_title=fr_first_title,
-                        fr_second_title=fr_second_title,
+                        fr_third_title=fr_third_title,
                         fr_button=fr_button,
                         ar_first_title=ar_first_title,
-                        ar_second_title=ar_second_title,
+                        ar_third_title=ar_third_title,
                         ar_button=ar_button,
                         link=link,
                         thumb=thumb,
