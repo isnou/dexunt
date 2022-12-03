5c1b386440bf2ccb41b5bf856a626e8bb7b0b5f2
isnou <nouioua0islem@gmail.com>
2022-12-03 15:20:54 +0100

    Update admin.py

--- src/main_shop/admin.py
+++ src/main_shop/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Layout
 
-admin.site.register(Layout)
+
+class LayoutAdmin(admin.ModelAdmin):
+    list_display = ('en_first_title', 'thumb', 'rank')
+
+
+admin.site.register(Layout, LayoutAdmin)
+
