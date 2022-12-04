4bff7655c20ec247cfb841c837c8fe72d46c5da1
isnou <nouioua0islem@gmail.com>
2022-12-04 12:04:04 +0100

    d

--- src/shop_manager/context_processors.py
+++ src/shop_manager/context_processors.py
@@ -8,8 +8,10 @@ def shop_manager_content(request):
         raise Http404("No products")
     products = raw_products_list.order_by('en_product_title', 'en_variant')
     inventory_product_count = raw_products_list.count()
+    showcase_products = raw_products_list.filter(type='main')
     return {
         'products': products,
         'inventory_product_count': inventory_product_count,
+        'showcase_products': showcase_products,
         'fool': [0, 1, 2, 3, 4],
     }
