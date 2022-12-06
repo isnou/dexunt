f8d576d3a6c77d5d7c380ac1f3527897fcb73352
isnou <nouioua0islem@gmail.com>
2022-12-06 10:05:53 +0100

    Update views.py

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -7,6 +7,5 @@ def main_shop_home(request, action):
     direction = request.session.get('language')
     url = direction + "/main-shop/base.html"
     context = {
-        'lang': lang,
     }
     return render(request, url, context)
