8fd82d13c9b5b3653fd9374c5d969405547e4ccd
isnou <nouioua0islem@gmail.com>
2022-12-06 11:00:55 +0100

    m

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render, redirect
 def main_shop_home(request, action):
     if not request.session.get('language', None):
         request.session['language'] = 'en'
+    if action == 'en':
+        request.session['language'] = 'en'
+    if action == 'fr':
+        request.session['language'] = 'fr'
+    if action == 'ar':
+        request.session['language'] = 'ar'
     direction = request.session.get('language')
     url = direction + "/main-shop/base.html"
     context = {
