f2612d7b171889da5208a18aece83ffce545ef25
isnou <nouioua0islem@gmail.com>
2022-12-07 11:38:26 +0100

    s

--- src/main_shop/urls.py
+++ src/main_shop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home-page/', views.main_shop_home, name='main-shop-home'),
+    path('', views.main_shop_home, name='main-shop-home'),
     path('<str:language>/', views.change_language, name='change-language'),
 ]
