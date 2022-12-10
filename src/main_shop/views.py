7a782809514ff6011523aae97019d105a8f69c94
isnou <nouioua0islem@gmail.com>
2022-12-10 08:28:05 +0100

    Update views.py

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -27,7 +27,7 @@ def product(request, sku):
     related_products = Product.objects.all().filter(en_product_title=selected_product.en_product_title)
     selected_variants = related_products.filter(type='main').exclude(en_variant=selected_product.en_variant)
 
-    album = related_products.filter(en_variant=selected_product.en_variant + ' photo').filter(type='photo')
+    album = related_products.filter(en_variant=selected_product.en_variant + ' photo')
 
     direction = request.session.get('language')
     url = direction + "/main-shop/product.html"
