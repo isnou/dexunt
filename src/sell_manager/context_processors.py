59f3b9151a7769cad8ac8a90d4f8ac06b1a2b0e9
isnou <nouioua0islem@gmail.com>
2022-12-11 23:18:57 +0100

    Update context_processors.py

--- src/sell_manager/context_processors.py
+++ src/sell_manager/context_processors.py
@@ -18,7 +18,7 @@ def clips_manager(request):
     else:
         points = Clip()
 
-    points_added_products = Product.objects.all().exclude()
+    points_added_products = Product.objects.all().filter(type='points-products')
     points_products_to_add = products
 
     if raw_clips.filter(type='points-products').exists():
