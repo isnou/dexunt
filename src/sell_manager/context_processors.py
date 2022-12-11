170fc9c0cedc8223c526f6ff12b9aead751a8c1a
isnou <nouioua0islem@gmail.com>
2022-12-11 13:35:38 +0100

    a

--- src/sell_manager/context_processors.py
+++ src/sell_manager/context_processors.py
@@ -5,7 +5,7 @@ def shop_manager_content(request):
     try:
         raw_clips = Clip.objects.all()
     except Clip.DoesNotExist:
-        raise Http404("No products")
+        raise Http404("No clips")
 
     if raw_clips.filter(type='points').exists():
         points = raw_clips.get(type='points')
