32bc945ef37d098b0ec3d96aabd840d595488d0d
isnou <nouioua0islem@gmail.com>
2022-12-11 13:14:41 +0100

    Update context_processors.py

--- src/sell_manager/context_processors.py
+++ src/sell_manager/context_processors.py
@@ -7,9 +7,10 @@ def shop_manager_content(request):
     except Clip.DoesNotExist:
         raise Http404("No products")
 
-    points = raw_clips.filter(type='points').order_by('-product_title')
-    point_count = points.count()
+    if raw_clips.filter(type='points').exists():
+        points = raw_clips.get(type='points')
+    else:
+        points = None
     return {
         'points': points,
-        'point_count': point_count,
     }
