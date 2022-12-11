6535ab6a8d9ae3e7937e4f60b385b078fabbfadf
isnou <nouioua0islem@gmail.com>
2022-12-11 15:26:34 +0100

    d

--- src/sell_manager/context_processors.py
+++ src/sell_manager/context_processors.py
@@ -1,4 +1,5 @@
 from .models import Clip
+from shop_manager.models import Product
 
 
 def clips_manager(request):
@@ -7,10 +8,27 @@ def clips_manager(request):
     except Clip.DoesNotExist:
         raise Http404("No clips")
 
+    try:
+        products = Product.objects.all().filter(type='main')
+    except Product.DoesNotExist:
+        raise Http404("No products")
+
     if raw_clips.filter(type='points').exists():
         points = raw_clips.get(type='points')
     else:
         points = Clip()
+
+    points_added_products = None
+    points_products_to_add = products
+
+    if raw_clips.filter(type='points-products').exists():
+        all_points = raw_clips.filter(type='points-products')
+        for points in all_points:
+            points_added_products.add(products.get(sku=points.sku))
+            points_products_to_add = products.exclude(sku=points.sku)
+
     return {
         'points': points,
+        'points_added_products': points_added_products,
+        'points_products_to_add': points_products_to_add,
     }
