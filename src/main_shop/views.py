c70b53c5b5a9389775586baf1dc4c7a7909847e9
isnou <nouioua0islem@gmail.com>
2022-12-15 19:41:59 +0100

    sd

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -53,7 +53,7 @@ def product(request, sku):
 
     selected_product = Product.objects.all().get(sku=sku)
     related_products = Product.objects.all().filter(en_product_title=selected_product.en_product_title)
-    selected_variants = related_products.filter(type='proto_variant')
+    selected_variants = related_products.exclude(type='photo').exclude(type='size')
 
     album = related_products.filter(type='photo').filter(attach=selected_product.attach)
     size_variants = related_products.filter(type='size').filter(attach=selected_product.attach)
