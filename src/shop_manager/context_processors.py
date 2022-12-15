27cd6ff3e841adc9ba1db0d6250fd6b4910011f5
isnou <nouioua0islem@gmail.com>
2022-12-15 18:44:51 +0100

    d

--- src/shop_manager/context_processors.py
+++ src/shop_manager/context_processors.py
@@ -8,10 +8,12 @@ def shop_manager_content(request):
         raise Http404("No products")
     products = raw_products_list.order_by('en_product_title', 'en_variant', '-updated_at')
     inventory_product_count = raw_products_list.count()
-    showcase_products = raw_products_list.filter(type='main') + raw_products_list.filter(type='proto')
-    best_sellers = raw_products_list.filter(type='main').filter(type='proto').order_by('-sell_rate')[:4]
-    top_rated = raw_products_list.filter(type='main').filter(type='proto').order_by('-review_rate')[:4]
-    new_arrivals = raw_products_list.filter(type='main').filter(type='proto').order_by('-created_at')[:4]
+    collection = raw_products_list.exclude(type='variant').exclude(type='proto_variant').exclude(type='set')\
+        .exclude(type='photo').exclude(type='size')
+    showcase_products = collection.order_by('-updated_at')
+    best_sellers = collection.order_by('-sell_rate')[:4]
+    top_rated = collection.order_by('-review_rate')[:4]
+    new_arrivals = collection.order_by('-created_at')[:4]
     return {
         'products': products,
         'inventory_product_count': inventory_product_count,
