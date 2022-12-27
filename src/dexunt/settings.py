98dc7a55d4937d17f6e6dc9cbbc5178d5edfa108
isnou <nouioua0islem@gmail.com>
2022-12-27 14:53:36 +0100

    a

--- src/dexunt/settings.py
+++ src/dexunt/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_user_agents',
     'phonenumber_field',
     'mathfilters',
     'sell_manager',
