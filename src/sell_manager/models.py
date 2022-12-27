c427d793936f68d831efb123f0a0ccaf7fc27c08
isnou <nouioua0islem@gmail.com>
2022-12-27 13:54:24 +0100

    a

--- src/sell_manager/models.py
+++ src/sell_manager/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from shop_manager.models import Product
+from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -30,15 +31,44 @@ class Clip(models.Model):
 
 class Collection(models.Model):
     # --------------------------------- collection technical informations ----------------------
-    sku = models.CharField(max_length=30, blank=True, null=True)
-    clips = models.ManyToManyField(Clip, blank=True)
+    solidarity = models.IntegerField(default=0)
+    delivery = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
     # --------------------------------- media --------------------------------------------------
     thumb = models.ImageField(upload_to='sell-manager/collection/thumb', blank=True, null=True)
     # --------------------------------- info ---------------------------------------------------
-    product_name = models.CharField(max_length=300, blank=True, null=True)
+    product_sku = models.CharField(max_length=30, blank=True, null=True)
+    en_product_name = models.CharField(max_length=300, blank=True, null=True)
+    fr_product_name = models.CharField(max_length=300, blank=True, null=True)
+    ar_product_name = models.CharField(max_length=300, blank=True, null=True)
     product_option = models.CharField(max_length=300, blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.product_name
+        return self.en_product_name
+
+
+class Cart(models.Model):
+    # --------------------------------- cart technical informations ----------------------------
+    ref = models.CharField(max_length=30, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    device = models.CharField(max_length=200, default='UNDEFINED')
+    operating_system = models.CharField(max_length=200, default='UNDEFINED')
+    ip_address = models.CharField(max_length=200, default='UNDEFINED')
+    # --------------------------------- info ---------------------------------------------------
+    collections = models.ManyToManyField(Collection, blank=True)
+    client_name = models.CharField(max_length=300, blank=True, null=True)
+    client_phone = PhoneNumberField(blank=True)
+
+
+class Order(models.Model):
+    # --------------------------------- order technical informations ---------------------------
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cart_ref = models.CharField(max_length=30, blank=True, null=True)
+    # --------------------------------- info ---------------------------------------------------
+    collections = models.ManyToManyField(Collection, blank=True)
+    client_name = models.CharField(max_length=300, blank=True, null=True)
+    client_phone = PhoneNumberField(blank=True)
