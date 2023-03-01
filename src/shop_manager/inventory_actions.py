4a54073f440cdb6dd54801ad88e51452d5d74bdc
isnou <nouioua0islem@gmail.com>
2023-03-01 19:29:42 +0100

    a

--- src/shop_manager/inventory_actions.py
+++ src/shop_manager/inventory_actions.py
@@ -52,8 +52,7 @@ def add_new_product(request):
         new_product.publish = True
         new_product.type = 'main'
         new_product.save()
-        Collection(en_title=new_product.en_title, ).save()
-        Collection.objects.all().get(en_title=new_product.en_title).product.add(new_product)
+        Collection(en_title=new_product.en_title, ).save().product.add(new_product)
 
     return {
         'url': url,
