3965513564214a9fca8bb9f72260260b832547a3
isnou <nouioua0islem@gmail.com>
2023-03-01 18:33:12 +0100

    a

--- src/shop_manager/admin.py
+++ src/shop_manager/admin.py
@@ -8,10 +8,5 @@ class ProductAdmin(admin.ModelAdmin):
                     'get_features', 'review_rate', 'sell_rate')
 
 
-class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('en_title', 'sizes', 'colors', 'sets')
-
-
 admin.site.register(Feature)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Collection, CollectionAdmin)
