be0ffd56109a429a6b7a326873f5121963096001
isnou <nouioua0islem@gmail.com>
2023-03-02 21:25:08 +0100

    a

--- src/shop_manager/admin.py
+++ src/shop_manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Feature, Product
+from .models import Feature, Product, Size
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -8,4 +8,5 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Feature)
+admin.site.register(Size)
 admin.site.register(Product, ProductAdmin)
