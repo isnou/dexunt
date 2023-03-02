27c4c938d2ddae656e7b1d8a7179e7a7e4286f85
isnou <nouioua0islem@gmail.com>
2023-03-02 19:59:02 +0100

    Update models.py

--- src/shop_manager/models.py
+++ src/shop_manager/models.py
@@ -46,7 +46,7 @@ class Size(models.Model):
     discount_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
-        return self.en_title
+        return self.value
 
 
 class Product(models.Model):
