06812a9be3959fa759816621abdec7bbbb3673ae
isnou <nouioua0islem@gmail.com>
2023-03-03 15:22:19 +0100

    a

--- src/shop_manager/admin.py
+++ src/shop_manager/admin.py
@@ -3,7 +3,7 @@ from .models import Feature, Product, Size
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('en_title', 'publish', 'upc', 'sku', 'tag', 'sizes', 'quantity', 'created_at',
+    list_display = ('en_title', 'publish', 'upc', 'sku', 'tag', 'quantity', 'created_at',
                     'updated_at', 'buy_price', 'sell_price', 'discount_price', 'features', 'review_rate', 'sell_rate')
 
 
