9bd5fa370e82ad0b9993de2a887bfb28bc89e349
isnou <nouioua0islem@gmail.com>
2023-03-04 00:50:06 +0100

    Update models.py

--- src/shop_manager/models.py
+++ src/shop_manager/models.py
@@ -108,8 +108,6 @@ class ShowcaseProduct(models.Model):
     sell_rate = models.IntegerField(default=0)
     # --------------------------------- showcase information -----------------------------------
     product = models.ManyToManyField(Product, blank=True)
-    brand = models.CharField(max_length=80, blank=True, null=True)
-    model = models.CharField(max_length=80, blank=True, null=True)
     sell_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     discount_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
 
