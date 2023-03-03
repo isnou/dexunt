070b3bebdcae56bd072319b5865fd50c9517ea7d
isnou <nouioua0islem@gmail.com>
2023-03-03 14:23:06 +0100

    a

--- src/shop_manager/admin.py
+++ src/shop_manager/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Feature, Product, Size
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('en_title', 'publish', 'upc', 'sku', 'tag', 'quantity', 'created_at',
+                    'updated_at', 'buy_price', 'sell_price', 'discount_price', 'review_rate', 'sell_rate')
+
+
 admin.site.register(Feature)
 admin.site.register(Size)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
