5cc7582d741df1d7f53e63b81db00dc5d2571d31
isnou <nouioua0islem@gmail.com>
2023-03-04 16:11:50 +0100

    ax

--- src/shop_manager/context_processors.py
+++ src/shop_manager/context_processors.py
@@ -6,14 +6,13 @@ def shop_manager_content(request):
         raw_products_list = Product.objects.all()
     except InventoryProduct.DoesNotExist:
         raise Http404("No products")
-    products = raw_products_list.order_by('en_title')
-    collection = raw_products_list.exclude(publish=False)
-    showcase_products = collection.order_by('-updated_at')
-    best_sellers = collection.order_by('-sell_rate')[:4]
-    top_rated = collection.order_by('-review_rate')[:4]
-    new_arrivals = collection.order_by('-created_at')[:4]
+    inventory_products = raw_products_list.order_by('en_title')
+    showcase_products = raw_products_list.exclude(publish=False)
+    best_sellers = showcase_products.order_by('-sell_rate')[:4]
+    top_rated = showcase_products.order_by('-review_rate')[:4]
+    new_arrivals = showcase_products.order_by('-created_at')[:4]
     return {
-        'products': products,
+        'inventory_products': inventory_products,
         'showcase_products': showcase_products,
         'best_sellers': best_sellers,
         'top_rated': top_rated,
