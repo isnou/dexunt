24fc5e44770465db1c3c261bf19a65d1e730b87c
isnou <nouioua0islem@gmail.com>
2023-03-06 13:40:59 +0100

    a

--- src/shop_manager/context_processors.py
+++ src/shop_manager/context_processors.py
@@ -1,21 +1,15 @@
-from .models import Product
+from .models import Product, ShowcaseProduct
 
 
 def shop_manager_content(request):
     try:
-        raw_products_list = Product.objects.all()
-    except Product.DoesNotExist:
+        shop_products_list = ShowcaseProduct.objects.all()
+    except ShowcaseProduct.DoesNotExist:
         raise Http404("No products")
-    inventory_products = raw_products_list.order_by('en_title')
-    showcase_products = raw_products_list.exclude(publish=False)
-    best_sellers = showcase_products.order_by('-sell_rate')[:4]
-    top_rated = showcase_products.order_by('-review_rate')[:4]
-    new_arrivals = showcase_products.order_by('-updated_at')[:4]
+    #showcase_products = raw_products_list.exclude(publish=False)
+    #best_sellers = showcase_products.order_by('-sell_rate')[:4]
+    #top_rated = showcase_products.order_by('-review_rate')[:4]
+    #new_arrivals = showcase_products.order_by('-updated_at')[:4]
     return {
-        'inventory_products': inventory_products,
-        'showcase_products': showcase_products,
-        'best_sellers': best_sellers,
-        'top_rated': top_rated,
-        'new_arrivals': new_arrivals,
-        'fool': [0, 1, 2, 3, 4],
+        
     }
