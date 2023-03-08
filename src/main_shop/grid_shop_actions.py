c3e5f9c348367389cf0a6e5ce2fc875a6c4172c7
isnou <nouioua0islem@gmail.com>
2023-03-08 01:45:51 +0100

    Update grid_shop_actions.py

--- src/main_shop/grid_shop_actions.py
+++ src/main_shop/grid_shop_actions.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Product, Layout
+from .models import Product, IntroThumb, IntroBanner ,Showcase
 
 
 def all_products(request):
