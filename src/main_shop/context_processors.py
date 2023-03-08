8b59378eb0a62e6482e3e0f5760d2abbed88b6b2
isnou <nouioua0islem@gmail.com>
2023-03-08 01:52:26 +0100

    Update context_processors.py

--- src/main_shop/context_processors.py
+++ src/main_shop/context_processors.py
@@ -2,49 +2,16 @@ from .models import IntroBanner, IntroThumb, Showcase
 
 
 def main_shop_content(request):
-    try:
-        layouts = Layout.objects.all()
-    except InventoryProduct.DoesNotExist:
-        raise Http404("No products")
-    if layouts.filter(type='first_main_banner').exists():
-        first_main_banner = layouts.get(type='first_main_banner')
-    else:
-        first_main_banner = Layout()
-    if layouts.filter(type='second_main_banner').exists():
-        second_main_banner = layouts.get(type='second_main_banner')
-    else:
-        second_main_banner = Layout()
-    if layouts.filter(type='third_main_banner').exists():
-        third_main_banner = layouts.get(type='third_main_banner')
-    else:
-        third_main_banner = Layout()
-    if layouts.filter(type='first_thumb_banner').exists():
-        first_thumb_banner = layouts.get(type='first_thumb_banner')
-    else:
-        first_thumb_banner = Layout()
-    if layouts.filter(type='second_thumb_banner').exists():
-        second_thumb_banner = layouts.get(type='second_thumb_banner')
-    else:
-        second_thumb_banner = Layout()
-    if layouts.filter(type='third_thumb_banner').exists():
-        third_thumb_banner = layouts.get(type='third_thumb_banner')
-    else:
-        third_thumb_banner = Layout()
-    if layouts.filter(type='showcase').exists():
-        showcases = layouts.filter(type='showcase').order_by('-rank')
-        showcases_exists = True
-    else:
-        showcases = Layout()
-        showcases_exists = False
+
     return {
-        'first_main_banner': first_main_banner,
-        'second_main_banner': second_main_banner,
-        'third_main_banner': third_main_banner,
+        'first_main_banner': None,
+        'second_main_banner': None,
+        'third_main_banner': None,
 
-        'first_thumb_banner': first_thumb_banner,
-        'second_thumb_banner': second_thumb_banner,
-        'third_thumb_banner': third_thumb_banner,
+        'first_thumb_banner': None,
+        'second_thumb_banner': None,
+        'third_thumb_banner': None,
 
-        'showcases_exists': showcases_exists,
-        'showcases': showcases,
+        'showcases_exists': None,
+        'showcases': None,
     }
