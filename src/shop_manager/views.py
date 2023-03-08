fb16096292dd9c90bdc19531f9f824f156f6ec84
isnou <nouioua0islem@gmail.com>
2023-03-08 16:18:02 +0100

    ss

--- src/shop_manager/views.py
+++ src/shop_manager/views.py
@@ -142,8 +142,10 @@ def e_shop(request, action, detail, index):
         url = direction + e_shop_actions.edit_banner(request, index).get('url')
     if action == "up_banner":
         url = direction + e_shop_actions.up_banner().get('url')
+        intro_banners = e_shop_actions.initialisation().get('intro_banners').order_by('rank')
     if action == "down_banner":
         url = direction + e_shop_actions.down_banner().get('url')
+        intro_banners = e_shop_actions.initialisation().get('intro_banners').order_by('rank')
     if action == "edit_thumb_banner":
         url = direction + e_shop_actions.thumb_banner(request, detail).get('url')
     if action == "add_movable_banner":
