2e9c9a7ab96869730ae9d9ccd41193ce378ab179
isnou <nouioua0islem@gmail.com>
2023-03-10 22:23:20 +0100

    s

--- src/shop_manager/context_processors.py
+++ src/shop_manager/context_processors.py
@@ -6,10 +6,16 @@ def shop_manager_content(request):
         shop_products_list = ShowcaseProduct.objects.all()
     except ShowcaseProduct.DoesNotExist:
         raise Http404("No products")
+
+    try:
+        all_products = Product.objects.all()
+    except Product.DoesNotExist:
+        raise Http404("No products")
+
     #showcase_products = raw_products_list.exclude(publish=False)
     #best_sellers = showcase_products.order_by('-sell_rate')[:4]
     #top_rated = showcase_products.order_by('-review_rate')[:4]
     #new_arrivals = showcase_products.order_by('-updated_at')[:4]
     return {
-        
+        'products' : products,
     }
