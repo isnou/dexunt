541c4f9014eba7b3027de281be07cce769a3c9b0
isnou <nouioua0islem@gmail.com>
2023-03-13 18:15:59 +0100

    hh

--- src/shop_manager/context_processors.py
+++ src/shop_manager/context_processors.py
@@ -16,6 +16,7 @@ def shop_manager_content(request):
     best_sellers = published_products.order_by('-sell_rate')[:4]
     top_rated = published_products.order_by('-review_rate')[:4]
     new_arrivals = published_products.order_by('-updated_at')[:4]
+
     return {
         'all_products' : all_products,
         'best_sellers' : best_sellers,
