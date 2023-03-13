94adab0cdb4359ec078fb046121e137087646d6f
isnou <nouioua0islem@gmail.com>
2023-03-13 21:51:03 +0100

    ss

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect
-from .models import Product ,ShowcaseProduct
+from .models import Product
 from sell_manager.models import Clip
+from main_shop.models import ShowcaseProduct
 from .models import Showcase
 from . import grid_shop_actions, product_actions
 
