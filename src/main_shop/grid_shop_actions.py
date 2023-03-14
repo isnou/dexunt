675f829234104669d95d0c634067b1b51ade8f0d
isnou <nouioua0islem@gmail.com>
2023-03-14 21:05:09 +0100

    ss

--- src/main_shop/grid_shop_actions.py
+++ src/main_shop/grid_shop_actions.py
@@ -1,11 +1,29 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Product, IntroThumb, IntroBanner ,Showcase
+from .models import Product
 
 
 def all_products(request):
     url = "/main-shop/grid-shop.html"
     products = Product.objects.all().exclude(type='variant').exclude(type='proto_variant').exclude(type='set')\
         .exclude(type='photo').exclude(type='size')
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products, 4)
+    try:
+        products_list = paginator.page(page)
+    except PageNotAnInteger:
+        products_list = paginator.page(1)
+    except EmptyPage:
+        products_list = paginator.page(paginator.num_pages)
+
+    return {
+        'url': url,
+        'products_list': products_list,
+    }
+
+def best_sellers(request):
+    url = "/main-shop/grid-shop.html"
+    products = Product.objects.all().exclude(publish=False).order_by('-sell_rate')
 
     page = request.GET.get('page', 1)
     paginator = Paginator(products, 4)
