6be1832dcb7a3c2190402e03e94cc1f2d9ac9430
isnou <nouioua0islem@gmail.com>
2023-03-15 14:38:06 +0100

    ss

--- src/shop_manager/views.py
+++ src/shop_manager/views.py
@@ -225,7 +225,8 @@ def e_shop(request, action, detail, index):
     if action == "edit_category":
         url = direction + e_shop_actions.edit_category(request, detail).get('url')
         tab = 'category'
-        sub_tab = detail
+        sub_tab = Directory.objects.all().get(id=index).sku
+        second_sub_tab = detail
     if action == "activate_category":
         category = Category.objects.all().get(sku=detail)
         category.publish = True
