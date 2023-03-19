785a31fb7640e16415335230aa4de4b8472bd3a9
isnou <nouioua0islem@gmail.com>
2023-03-19 02:40:29 +0100

    ss

--- src/main_shop/urls.py
+++ src/main_shop/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main_shop_home, name='main-shop-home'),
-    path('<str:language>/', views.change_language, name='change-language'),
+    path('<str:language>/change-language/', views.change_language, name='change-language'),
     path('<str:sku>/<str:size_sku>/single-product/', views.product, name='single-product'),
     path('<str:action>/<int:ref>/grid-shop/', views.grid_shop, name='grid-shop'),
 ]
