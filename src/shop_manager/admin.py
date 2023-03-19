d1213ecfcaa720ba3658239d43c61192a124023b
isnou <nouioua0islem@gmail.com>
2023-03-19 01:34:02 +0100

    ee

--- src/shop_manager/admin.py
+++ src/shop_manager/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Feature, Product, Size, ShowcaseProduct
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('en_title', 'publish', 'upc', 'sku', 'tag', 'quantity', 'created_at',
+                    'updated_at', 'buy_price', 'sell_price', 'discount_price', 'features', 'review_rate', 'sell_rate')
+
 
 admin.site.register(Feature)
 admin.site.register(Size)
