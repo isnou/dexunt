989847265886b824b65e7df08fa6bb60dcac2ab8
isnou <nouioua0islem@gmail.com>
2023-03-19 17:25:58 +0100

    dd

--- src/main_shop/urls.py
+++ src/main_shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_shop_home, name='main-shop-home'),
+    path('main/', views.main_shop_home, name='main-shop-home'),
     path('<str:language>/change-language/', views.change_language, name='change-language'),
     path('<str:sku>/<str:size_sku>/single-product/', views.product, name='single-product'),
     path('<str:action>/<int:ref>/grid-shop/', views.grid_shop, name='grid-shop'),
