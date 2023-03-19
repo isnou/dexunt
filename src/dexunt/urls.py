44a9a2d43efa0114238ac1f4595f8900adb74897
isnou <nouioua0islem@gmail.com>
2023-03-19 02:34:41 +0100

    dd

--- src/dexunt/urls.py
+++ src/dexunt/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('main_shop.urls')),
+    path('main-shop/', include('main_shop.urls')),
     path('shop-manager/', include('shop_manager.urls')),
     path('admin/', admin.site.urls),
 ]
