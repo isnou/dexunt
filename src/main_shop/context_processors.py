60fa018ed191455a745cdca22cf78f4d580172f2
isnou <nouioua0islem@gmail.com>
2023-03-20 19:29:06 +0100

    ss

--- src/main_shop/context_processors.py
+++ src/main_shop/context_processors.py
@@ -1,10 +1,24 @@
 from .models import Intro, Showcase, Category, Directory, Department
-
+from sell_manager.models import Cart
+from add_ons import functions
 
 
 def main_shop_content(request):
     intro = Intro.objects.all().get(id=1)
 
+    if not request.session.get('cart', None):
+        request.session['cart'] = functions.serial_number_generator(30).upper()
+    cart_ref = request.session.get('cart')
+
+    if Cart.objects.all().filter(ref=cart_ref).exists():
+        cart = Cart.objects.all().get(ref=cart_ref)
+    else:
+        cart = Cart(ref=cart_ref,
+                    device=request.user_agent.device.family,
+                    operating_system=request.user_agent.os.family + request.user_agent.os.version_string,
+                    ip_address=request.META['REMOTE_ADDR'],
+                    )
+        cart.save()
 
     try:
         showcases = Showcase.objects.all().order_by('-rank')
@@ -18,6 +32,7 @@ def main_shop_content(request):
 
     return {
         'intro': intro,
+        'cart': cart,
         'showcases': showcases,
         'departments': departments,
     }
--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect
 from shop_manager.models import ShowcaseProduct, Product
-from sell_manager.models import Cart
+
 from .models import Showcase
 from . import grid_shop_actions
 from add_ons import functions
@@ -10,25 +10,11 @@ from add_ons import functions
 def main_shop_home(request):
     if not request.session.get('language', None):
         request.session['language'] = 'en'
-    if not request.session.get('cart', None):
-        request.session['cart'] = functions.serial_number_generator(30).upper()
+        
     direction = request.session.get('language')
     url = direction + "/main-shop/main-page.html"
 
-    cart_ref = request.session.get('cart')
-
-    if Cart.objects.all().filter(ref=cart_ref).exists():
-        cart = Cart.objects.all().get(ref=cart_ref)
-    else:
-        cart = Cart(ref=cart_ref,
-                    device=request.user_agent.device.family,
-                    operating_system=request.user_agent.os.family+request.user_agent.os.version_string,
-                    ip_address=request.META['REMOTE_ADDR'],
-                    )
-        cart.save()
-
     context = {
-        'cart': cart
     }
     return render(request, url, context)
 
