4359eba77a3ae2b5e12a972504865b0efd2c245b
isnou <nouioua0islem@gmail.com>
2023-03-20 17:35:14 +0100

    ss

--- src/sell_manager/context_processors.py
+++ src/sell_manager/context_processors.py
@@ -1,42 +1,8 @@
-from .models import Clip
 # from shop_manager.models import Product
 
 
 def clips_manager(request):
-    try:
-        raw_clips = Clip.objects.all()
-    except Clip.DoesNotExist:
-        raise Http404("No clips")
-    if raw_clips.filter(type='points').exists():
-        points = raw_clips.get(type='points')
-    else:
-        points = Clip()
-    if raw_clips.filter(type='delivery').exists():
-        delivery = raw_clips.get(type='delivery')
-    else:
-        delivery = Clip()
-    if raw_clips.filter(type='solidarity').exists():
-        solidarity = raw_clips.get(type='solidarity')
-    else:
-        solidarity = Clip()
-    if raw_clips.filter(type='points-products').exists():
-        points_products = raw_clips.filter(type='points-products').order_by('product_title')
-    else:
-        points_products = None
-    if raw_clips.filter(type='delivery-products').exists():
-        delivery_products = raw_clips.filter(type='delivery-products').order_by('product_title')
-    else:
-        delivery_products = None
-    if raw_clips.filter(type='solidarity-products').exists():
-        solidarity_products = raw_clips.filter(type='solidarity-products').order_by('product_title')
-    else:
-        solidarity_products = None
+
 
     return {
-        'points': points,
-        'delivery': delivery,
-        'solidarity': solidarity,
-        'points_products': points_products,
-        'delivery_products': delivery_products,
-        'solidarity_products': solidarity_products,
     }
