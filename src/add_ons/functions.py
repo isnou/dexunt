10e471e65ddf25eefe6ebf3c2c2f38f44c55cd39
isnou <nouioua0islem@gmail.com>
2023-03-20 18:12:38 +0100

    Update functions.py

--- src/add_ons/functions.py
+++ src/add_ons/functions.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 def serial_number_generator(length):
     letters_and_digits = string.ascii_letters + string.digits
     result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
