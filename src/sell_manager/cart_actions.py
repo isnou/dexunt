31f2316379fbc6216eb17a14e1b70db304763aa6
isnou <nouioua0islem@gmail.com>
2023-03-20 22:25:00 +0100

    ss

--- src/sell_manager/cart_actions.py
+++ src/sell_manager/cart_actions.py
@@ -12,23 +12,32 @@ def add_product_to_cart(request):
         product_sku = request.POST.get('product_sku', False)
         quantity = request.POST.get('quantity', False)
 
-
-        if cart.product.all().filter(product_sku=product_sku).exists() and size_sku == 'main':
-            cart_product = cart.product.all().get(product_sku=product_sku)
-            cart_product.quantity = quantity
-            cart_product.save()
-        elif cart.product.all().filter(product_sku=product_sku).exists() and size_sku != 'main':
-            if cart.product.all().filter(size_sku=size_sku).exists():
-                cart_product = cart.product.all().get(size_sku=size_sku)
+        if cart.product.all().filter(product_sku=product_sku).exists():
+            if size_sku == 'main':
+                cart_product = cart.product.all().get(product_sku=product_sku)
                 cart_product.quantity = quantity
                 cart_product.save()
-        elif not cart.product.all().filter(product_sku=product_sku).exists() and size_sku == 'main':
+            else:
+                if cart.product.all().filter(size_sku=size_sku).exists():
+                    cart_product = cart.product.all().get(size_sku=size_sku)
+                    cart_product.quantity = quantity
+                    cart_product.save()
+                else:
+                    cart_product = CartProduct(product_sku=product_sku,
+                                               size_sku=size_sku,
+                                               quantity=int(quantity),
+                                               )
+                    cart_product.save()
+                    cart.product.add(cart_product)
+        else:
             cart_product = CartProduct(product_sku=product_sku,
                                        size_sku=size_sku,
                                        quantity=int(quantity),
                                        )
             cart_product.save()
             cart.product.add(cart_product)
+
+
 
     return {
         'url': url,
