4533ff9f09d6700f01fceac8e453107d284a69cd
isnou <nouioua0islem@gmail.com>
2023-03-22 00:32:01 +0100

    Update admin.py

--- src/sell_manager/admin.py
+++ src/sell_manager/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Cart ,CartProduct
+from .models import Cart ,CartProduct ,Destination ,SubDestination
 
 
 
 admin.site.register(Cart)
 admin.site.register(CartProduct)
+admin.site.register(Destination)
+admin.site.register(SubDestination)
