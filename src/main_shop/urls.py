eee9a9ccbb048a4d9d177dd7bc431d7cd09cee1e
isnou <nouioua0islem@gmail.com>
2023-03-21 15:04:28 +0100

    dd

--- src/main_shop/urls.py
+++ src/main_shop/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.main_shop_home, name='main-shop-home'),
     path('<str:language>/change-language/', views.change_language, name='change-language'),
     path('<str:sku>/<str:size_sku>/single-product/', views.product, name='single-product'),
-    path('<str:sku>/<str:size_sku>/cart-product/', views.cart_product, name='cart-product'),
     path('<str:action>/<int:ref>/grid-shop/', views.grid_shop, name='grid-shop'),
 ]
