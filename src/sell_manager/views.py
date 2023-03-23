54480ae9f4c1b940799d0797b37e5fb84ff7d70a
isnou <nouioua0islem@gmail.com>
2023-03-23 08:40:47 +0100

    Update views.py

--- src/sell_manager/views.py
+++ src/sell_manager/views.py
@@ -15,7 +15,8 @@ def cart_home(request, action):
     if action == 'add_product_to_cart':
         url = direction + cart_actions.add_product_to_cart(request).get('url')
         if cart_actions.add_product_to_cart(request).get('redirecting'):
-            action = 'show_cart'
+            url = direction + cart_actions.show_cart(request).get('url')
+            provinces = cart_actions.show_cart(request).get('provinces')
     if action == 'remove_product_from_cart':
         url = direction + cart_actions.remove_product_from_cart(request).get('url')
     if action == 'remove_quantity':
