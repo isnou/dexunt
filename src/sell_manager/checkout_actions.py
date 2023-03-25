d440d08b8266347214161b3f5f61821501898cfc
isnou <nouioua0islem@gmail.com>
2023-03-25 16:02:51 +0100

    ssd

--- src/sell_manager/checkout_actions.py
+++ src/sell_manager/checkout_actions.py
@@ -5,24 +5,24 @@ def details(request):
     url = "/main-shop/checkout-details.html"
     cart = Cart.objects.all().get(ref=request.session.get('cart'))
     earned_points = 0
+    shipping_price = 0
     province = None
     municipality = None
 
     if request.method == 'POST':
         province_en_name = request.POST.get('province_en_name', False)
         municipality_en_name = request.POST.get('municipality_en_name', False)
+        shipping_price = request.POST.get('shipping_price', False)
 
         province = Province.objects.all().get(en_name=province_en_name)
         municipality = Municipality.objects.all().get(en_name=municipality_en_name)
 
-    delivery_quotient = 0
-    item_count = 0
+
+
     for product in cart.product.all():
         earned_points += product.points * product.quantity
-        delivery_quotient += product.delivery * product.quantity
-        item_count += product.quantity
-    delivery_quotient = round(delivery_quotient / item_count)
-    shipping_price = round((municipality.home_delivery_price * delivery_quotient) / 100)
+
+    shipping_price = int(shipping_price)
 
 
 
