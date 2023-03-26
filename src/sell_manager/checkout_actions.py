56bd171d0039098db2338640d5daa9f92ab133d4
isnou <nouioua0islem@gmail.com>
2023-03-26 02:02:41 +0100

    ss

--- src/sell_manager/checkout_actions.py
+++ src/sell_manager/checkout_actions.py
@@ -64,32 +64,16 @@ def get_shipping_prices(request ,municipality_en_name):
 def review(request):
     url = "/main-shop/checkout-review.html"
     cart = Cart.objects.all().get(ref=request.session.get('cart'))
-    earned_points = 0
-    shipping_price = 0
-    province = None
-    municipality = None
-
+    
     if request.method == 'POST':
         province_en_name = request.POST.get('province_en_name', False)
         municipality_en_name = request.POST.get('municipality_en_name', False)
         shipping_price = request.POST.get('shipping_price', False)
 
-        province = Province.objects.all().get(en_name=province_en_name)
-        municipality = Municipality.objects.all().get(en_name=municipality_en_name)
 
-    for product in cart.product.all():
-        earned_points += product.points * product.quantity
-
-    shipping_price = int(shipping_price)
-    total_price = cart.sub_total_price + shipping_price
 
     context = {
         'cart': cart,
-        'earned_points': earned_points,
-        'shipping_price': shipping_price,
-        'province': province,
-        'municipality': municipality,
-        'total_price': total_price,
     }
 
     return {
