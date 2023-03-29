06d0b1729b0749b80c378edb4747c76aab260f5a
isnou <nouioua0islem@gmail.com>
2023-03-29 23:35:49 +0100

    ss

--- src/sell_manager/checkout_actions.py
+++ src/sell_manager/checkout_actions.py
@@ -84,8 +84,8 @@ def get_shipping_prices(request ,municipality_en_name):
         delivery_quotients += product.delivery
 
     delivery_quotient = round(delivery_quotients / cart.product.all().count())
-    home_delivery_price = round((municipality.home_delivery_price * delivery_quotient) / 1000) * 10
-    desk_delivery_price = round((municipality.desk_delivery_price * delivery_quotient) / 1000) * 10
+    home_delivery_price = round((municipality.home_delivery_price * delivery_quotient) / 10000) * 100
+    desk_delivery_price = round((municipality.desk_delivery_price * delivery_quotient) / 10000) * 100
 
     sub_context = {
         'home_delivery_price': home_delivery_price,
