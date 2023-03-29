c1a8ab4475cc4d4e7f74e1cb658889ef734a860f
isnou <nouioua0islem@gmail.com>
2023-03-30 00:59:35 +0100

    ss

--- src/sell_manager/checkout_actions.py
+++ src/sell_manager/checkout_actions.py
@@ -55,22 +55,29 @@ def review(request):
         province_en_name = request.POST.get('province_en_name', False)
         municipality_en_name = request.POST.get('municipality_en_name', False)
         shipping_price = request.POST.get('shipping_price', False)
+        shipping_type = request.POST.get('shipping_type', False)
         total_price = request.POST.get('total_price', False)
         earned_points = request.POST.get('earned_points', False)
         client_name = request.POST.get('client_name', False)
         phone_number = request.POST.get('phone_number', False)
+        coupon_code = request.POST.get('coupon_code', False)
 
+        coupon = check_promotion(coupon_code, total_price).get('coupon')
+        discounted_price = check_promotion(coupon_code, total_price).get('discounted_price')
 
         context = {
             'cart': cart,
+            'coupon': coupon,
             'province_en_name': province_en_name,
             'municipality_en_name': municipality_en_name,
+            'shipping_type': shipping_type,
             'client_name': client_name,
             'phone_number': phone_number,
 
             'earned_points': earned_points,
             'shipping_price': shipping_price,
             'total_price': total_price,
+            'discounted_price': discounted_price,
         }
     else:
         context = False
