1f49a20e04eb0843f161197bdf6a650c89a04a64
isnou <nouioua0islem@gmail.com>
2023-04-01 00:58:07 +0100

    jju

--- src/sell_manager/track_order_actions.py
+++ src/sell_manager/track_order_actions.py
@@ -7,7 +7,7 @@ def regular(request):
     cart = Cart.objects.all().get(ref=request.session.get('cart'))
 
     if request.method == 'POST':
-        order_ref = request.POST.get('order_ref', False)
+        order_ref = 'NJY7PQXCRIFL'
 
         if Order.objects.all().filter(order_ref=order_ref).exists():
             order = Order.objects.all().get(order_ref=order_ref)
