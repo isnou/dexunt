e640ffe796e6320123bae9d7944c80c5b514e55c
isnou <nouioua0islem@gmail.com>
2023-04-01 00:52:16 +0100

    dd

--- src/sell_manager/track_order_actions.py
+++ src/sell_manager/track_order_actions.py
@@ -8,13 +8,15 @@ def regular(request):
 
     if request.method == 'POST':
         order_ref = request.POST.get('order_ref', False)
-        order = Order.objects.all().get(order_ref=order_ref)
+
+        if Order.objects.all().filter(order_ref=order_ref).exists():
+            order = Order.objects.all().get(order_ref=order_ref)
+        else:
+            order = None
 
         context = {
             'order': order,
             'cart': cart,
         }
 
-        return {
-            'context': context,
-        }
+        return context
