69a4f95389c4dcfc48cf596cb22246d75de504cb
isnou <nouioua0islem@gmail.com>
2023-04-01 00:58:58 +0100

    Update track_order_actions.py

--- src/sell_manager/track_order_actions.py
+++ src/sell_manager/track_order_actions.py
@@ -7,7 +7,7 @@ def regular(request):
     cart = Cart.objects.all().get(ref=request.session.get('cart'))
 
     if request.method == 'POST':
-        order_ref = 'NJY7PQXCRIFL'
+        order_ref = request.POST.get('order_ref', False)
 
         if Order.objects.all().filter(order_ref=order_ref).exists():
             order = Order.objects.all().get(order_ref=order_ref)
@@ -16,7 +16,6 @@ def regular(request):
 
         context = {
             'order': order,
-            'cart': cart,
         }
 
         return {
