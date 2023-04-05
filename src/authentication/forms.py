a6bd706690179520a3262148da5f402826eb0423
isnou <nouioua0islem@gmail.com>
2023-04-06 00:19:46 +0100

    {% endfor %}

--- src/authentication/forms.py
+++ src/authentication/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from phonenumber_field.modelfields import PhoneNumberField
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='User Name')
+    username = forms.CharField(max_length=63, label='Username')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Password')
+    client_name = models.CharField(max_length=300, label='Name')
+    client_phone = PhoneNumberField()
--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,3 +1,4 @@
+from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,3 +6,8 @@ from django.db import models
 class User(AbstractUser):
     profile_photo = models.ImageField(verbose_name='profile photo')
     role = models.CharField(max_length=30, verbose_name='role')
+    client_name = models.CharField(max_length=300, blank=True, null=True)
+    client_phone = PhoneNumberField(blank=True)
+    points = models.IntegerField(default=0)
+    province = models.CharField(max_length=200, blank=True, null=True)
+    municipality = models.CharField(max_length=200, blank=True, null=True)
