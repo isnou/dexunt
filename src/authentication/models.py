3aaaa189f72a087f761202b0cb31e8f1b0a221b0
isnou <nouioua0islem@gmail.com>
2023-04-05 17:21:37 +0100

    Update models.py

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class User(AbstractUser):
     profile_photo = models.ImageField(verbose_name='profile photo')
     role = models.CharField(max_length=30, verbose_name='role')
+    banned = models.BooleanField(default=False)
