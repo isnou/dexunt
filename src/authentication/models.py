4f2cc488d5938adad01fcf2af78d2a85eef9e1f0
isnou <nouioua0islem@gmail.com>
2023-04-05 17:23:21 +0100

    linked = models.BooleanField(default=False)

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -5,4 +5,3 @@ from django.db import models
 class User(AbstractUser):
     profile_photo = models.ImageField(verbose_name='profile photo')
     role = models.CharField(max_length=30, verbose_name='role')
-    banned = models.BooleanField(default=False)
