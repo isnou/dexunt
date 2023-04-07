846b0ade1d5f7fd3b9cd3b0eb6a86e561c038f27
isnou <nouioua0islem@gmail.com>
2023-04-07 02:35:04 +0100

    id="{{ login_form.username.name }}" name="{{ login_form.username.name }}" maxlength="{{ login_form.username.field.max_length }}"

--- src/authentication/forms.py
+++ src/authentication/forms.py
@@ -6,7 +6,7 @@ class LoginForm(forms.Form):
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Password')
 
 class SignupForm(forms.Form):
-    client_name = forms.CharField(max_length=300, label='Name')
-    client_phone = forms.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
+    first_name = forms.CharField(max_length=300, label='Name')
+    phone_number = forms.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
     username = forms.CharField(max_length=63, label='Username')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Password')
--- src/authentication/models.py
+++ src/authentication/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 
 class User(AbstractUser):
-    client_name = models.CharField(max_length=300, blank=True, null=True)
-    client_phone = PhoneNumberField(blank=True)
+    first_name = models.CharField(max_length=300, blank=True, null=True)
+    last_name = models.CharField(max_length=300, blank=True, null=True)
+    phone_number = PhoneNumberField(blank=True)
     profile_photo = models.ImageField(verbose_name='profile photo')
     role = models.CharField(max_length=30, verbose_name='role')
     points = models.IntegerField(default=0)
     province = models.CharField(max_length=200, blank=True, null=True)
     municipality = models.CharField(max_length=200, blank=True, null=True)
+    activated_account = models.BooleanField(default=False)
