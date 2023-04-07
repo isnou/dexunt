adc7c4034637e45fe84ecf062b30bf8ca5bbd643
isnou <nouioua0islem@gmail.com>
2023-04-07 17:57:18 +0100

    Update models.py

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,7 +1,7 @@
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
 from sell_manager.models import Order, UserCart
-from main_shop.models import WishedProduct, NotifiedProduct, Server
+from .models import WishedProduct, NotifiedProduct, Server
 from django.db import models
 
 
