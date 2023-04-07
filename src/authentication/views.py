ca635f2ddfd31c230643c24f8a0c8884cb326c9c
isnou <nouioua0islem@gmail.com>
2023-04-07 12:17:30 +0100

    customer

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -35,6 +35,7 @@ def signup_page(request):
     if request.method == 'POST':
         signup_form = forms.SignupForm(request.POST)
         if form.is_valid():
+            signup_form.role = 'CUSTOMER'
             user = signup_form.save()
             # auto-login user
             login(request, user)
