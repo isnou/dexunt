5bd11b14811eb07b82910de54d7459ab760b3663
isnou <nouioua0islem@gmail.com>
2023-04-07 01:48:50 +0100

    login-page

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -9,11 +9,22 @@ def login_page(request):
         request.session['language'] = 'en'
 
     direction = request.session.get('language')
-    url = direction + "/main-shop/login-page.html"
+    url = direction + "/main-shop/main-page.html"
 
-    context = {
-    }
-    return render(request, url, context)
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user = authenticate(
+                username=login_form.cleaned_data['username'],
+                password=login_form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('user-home-page')
+            else:
+                context = {
+                }
+                return render(request, url, context)
 
 @login_required
 def user_home_page(request):
