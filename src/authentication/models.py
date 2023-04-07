b6c5c8a7317afdb764ab450a3a65fb3c14247e92
isnou <nouioua0islem@gmail.com>
2023-04-07 16:21:13 +0100

    <span class="text-accent fs-sm">your account is not yet activated</span>

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,9 +1,11 @@
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
+from sell_manager.models import Order
 from django.db import models
 
 
 class User(AbstractUser):
+    # --------------------------------- user info ----------------------------------------------
     first_name = models.CharField(max_length=300, blank=True, null=True)
     last_name = models.CharField(max_length=300, blank=True, null=True)
     phone_number = PhoneNumberField(blank=True)
@@ -13,3 +15,6 @@ class User(AbstractUser):
     province = models.CharField(max_length=200, blank=True, null=True)
     municipality = models.CharField(max_length=200, blank=True, null=True)
     activated_account = models.BooleanField(default=False)
+
+    # --------------------------------- order info ---------------------------------------------
+    order = models.ManyToManyField(Order, blank=True)
