cced31e40c1b268a9444a37855196e14843de9b1
isnou <nouioua0islem@gmail.com>
2023-04-07 12:08:01 +0100

    signup_form

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('login/page/', views.login_page, name='login-page'),
     path('user/page/', views.user_home_page, name='user-home-page'),
     path('user/logout/', views.user_logout, name='user-logout'),
+    path('signup/page/', views.signup_page, name='signup-page'),
+
 ]
