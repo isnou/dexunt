8c0d32c49db85af2975009a6796cb3a2d2fcfe7c
isnou <nouioua0islem@gmail.com>
2023-04-09 00:34:35 +0100

    context = {                 'check': True,                 'signup_form': signup_form,             }             return render(request, url, context)

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -104,7 +104,8 @@ def edit_profile(request):
     if request.method == 'POST':
         signup_form = SignupForm(request.POST, instance=request.user)
         if signup_form.is_valid():
-            signup_form.save()
+            pass
+            # signup_form.save()
         else:
             signup_form = SignupForm()
             context = {
