e3a79ff40f7c02e70319446b577294d731d4ed37
isnou <nouioua0islem@gmail.com>
2023-04-09 00:23:36 +0100

    signup_form = SignupForm()             context = {                 'check': True,                 'signup_form': signup_form,             }             return render(request, url, context)

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -103,11 +103,11 @@ def edit_profile(request):
 
     if request.method == 'POST':
         signup_form = SignupForm(request.POST, instance=request.user)
-        context = {
-            'check': True,
-            'signup_form': signup_form,
-        }
-        return render(request, url, context)
+        if signup_form.is_valid():
+            user = signup_form.save()
+            login(request, user)
+        else:
+            return redirect('account-orders-page')
 
 @login_required
 def change_password(request):
