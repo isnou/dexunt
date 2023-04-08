26749c3bdd80d7361681f585e26b4ef4e200501c
isnou <nouioua0islem@gmail.com>
2023-04-08 01:21:31 +0100

    orders

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from authentication.forms import LoginForm, SignupForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from . import forms
 
 
