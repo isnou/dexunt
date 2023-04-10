78aeee25abf569b122cef2b66fbf770ad42d980f
isnou <nouioua0islem@gmail.com>
2023-04-11 00:16:12 +0100

    1051, "Unknown tablev1051, "Unknown table

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
 from sell_manager.models import Order, UserCart
-from main_shop.models import WishedProduct, Server, BookedProduct
+from main_shop.models import Server
 from PIL import Image
 
 
