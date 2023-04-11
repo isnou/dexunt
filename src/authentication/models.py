844c65d4cb38f301210190109124df1db051de4c
isnou <nouioua0islem@gmail.com>
2023-04-11 02:12:12 +0100

    main_shop_notifiedproduct

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
 from sell_manager.models import Order, UserCart
-from main_shop.models import WishedProduct, Server, BookedProduct
+from main_shop.models import WishedProduct, Server
 from PIL import Image
 
 
@@ -23,7 +23,6 @@ class User(AbstractUser):
     cart = models.ManyToManyField(UserCart, blank=True)
     server = models.ManyToManyField(Server, blank=True)
     wished_product = models.ManyToManyField(WishedProduct, blank=True)
-    booked_product = models.ManyToManyField(BookedProduct, blank=True)
 
 
     def save(self, *args, **kwargs):
