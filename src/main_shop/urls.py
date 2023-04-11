1225158b7b2e3e3f2054eb5f0db32c144e8ceb26
isnou <nouioua0islem@gmail.com>
2023-04-11 03:48:53 +0100

    {% url 'book-it' sku size_sku %}

--- src/main_shop/urls.py
+++ src/main_shop/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main_shop_home, name='main-shop-home'),
-    path('<str:language>/change-language/', views.change_language, name='change-language'),
-    path('<str:sku>/<str:size_sku>/single-product/', views.product, name='single-product'),
-    path('<str:action>/<int:ref>/grid-shop/', views.grid_shop, name='grid-shop'),
+    path('<str:language>/change/language/', views.change_language, name='change-language'),
+    path('<str:sku>/<str:size_sku>/single/product/', views.product, name='single-product'),
+    path('<str:action>/<int:ref>/grid/shop/', views.grid_shop, name='grid-shop'),
+    path('<str:sku>/<str:size_sku>/book/it/', views.book_it, name='book-it'),
 ]
