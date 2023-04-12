8e1daaf8ac7a825f74ee42df1d57d9782cb5733f
isnou <nouioua0islem@gmail.com>
2023-04-12 23:56:37 +0100

    Update views.py

--- src/main_shop/views.py
+++ src/main_shop/views.py
@@ -188,7 +188,7 @@ def book_it(request, sku, size_sku):
 def un_book_it(request, sku):
     if request.user.is_authenticated:
         user = request.user
-        selected_product = user.booked.all().get(sku=sku)
+        selected_product = user.booked.all().get(product_sku=sku)
         selected_product.delete()
 
         return redirect('booked-products-page')
