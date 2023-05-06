081984d88857d3f504da35fb20e78babd098b5e7
isnou <nouioua0islem@gmail.com>
2023-05-06 17:28:18 +0100

    login_modal

--- src/content/views.py
+++ src/content/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render, redirect
 from add_ons import functions
+from authentication.forms import LoginForm, SignupForm
+from django.contrib.auth import login, authenticate
+
 
 
 def home_page(request):
     if not request.session.get('language', None):
         request.session['language'] = 'en-us'
-
     direction = request.session.get('language')
     url = direction + "/home/main-page.html"
+    login_form = LoginForm()
+    signup_form = SignupForm()
 
     context = {
+        'login_form': login_form,
+        'signup_form': signup_form,
     }
     return render(request, url, context)
 
+
+def management_page(request, action):
+    if action == 'show':
+        if not request.session.get('language', None):
+            request.session['language'] = 'en-us'
+        direction = request.session.get('language')
+        url = direction + "/management/admin-page.html"
+
+        return render(request, url, context)
+
+    
 def change_language(request):
     if request.method == 'POST':
         language = request.POST.get('language', False)
