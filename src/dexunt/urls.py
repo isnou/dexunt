9871f7c9b677b3582e9ffae6292c9bb18930cd05
isnou <nouioua0islem@gmail.com>
2023-05-06 17:47:47 +0100

    Update urls.py

--- src/dexunt/urls.py
+++ src/dexunt/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('content.urls')),
+    path('users/management/', include('authentication.urls')),
     path('admin/', admin.site.urls),
 ]
 
