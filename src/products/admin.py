34104071780923588a6918ab8a5ecd25a03bbd46
isnou <nouioua0islem@gmail.com>
2023-05-17 09:43:23 +0100

    (Product)

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Product, Variant, Option, Album
+
+admin.site.register(Product)
+admin.site.register(Variant)
+admin.site.register(Option)
+admin.site.register(Album)
