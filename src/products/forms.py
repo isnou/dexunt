9118faa871de62c1008c02a21e88083ad5509fc3
isnou <nouioua0islem@gmail.com>
2023-05-19 12:59:54 +0100

    number

--- src/products/forms.py
+++ src/products/forms.py
@@ -4,5 +4,5 @@ from .models import Product
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ('en_title', 'fr_title', 'ar_title', 'selected_image', 'brand', 'en_description', 'fr_description', 'ar_description', 'en_note', 'fr_note', 'ar_note')
+        fields = ('en_title', 'fr_title', 'ar_title', 'selected_image', 'brand', 'en_description', 'fr_description', 'ar_description', 'en_note', 'fr_note', 'ar_note', 'price', 'discount')
 
