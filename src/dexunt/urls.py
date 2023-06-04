be98f27233aeb023ce5037fd53969b88a265ec9c
isnou <nouioua0islem@gmail.com>
2023-06-04 22:14:52 +0100

    main_home

--- src/dexunt/urls.py
+++ src/dexunt/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('content.urls')),
+    path('', include('main_home.urls')),
     path('users/management/', include('authentication.urls')),
     path('admin/', admin.site.urls),
 ]
