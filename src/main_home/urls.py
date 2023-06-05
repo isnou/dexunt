d297eb3c1bb56d6080c5cd82ee4788929dcb2a58
isnou <nouioua0islem@gmail.com>
2023-06-05 23:23:12 +0100

    kk

--- src/main_home/urls.py
+++ src/main_home/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home-page'),
-    path('<str:action>/statistics/menu', views.statistics_menu, name='statistics-menu'),
-    path('<str:action>/products/menu', views.products_menu, name='products-menu'),
-    path('<str:action>/management/page', views.management_page, name='management-page'),
     path('change/language', views.change_language, name='change-language'),
 ]
