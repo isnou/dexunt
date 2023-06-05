c6628f6673b3d228ee649ad373761775fe158235
isnou <nouioua0islem@gmail.com>
2023-06-05 23:29:57 +0100

    manage-products

--- src/main_manager/urls.py
+++ src/main_manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<str:action>/statistics', views.statistics, name='statistics'),
     path('<str:action>/showcase', views.manage_showcase, name='manage-showcase'),
     path('<str:action>/products', views.manage_products, name='manage-products'),
 ]
