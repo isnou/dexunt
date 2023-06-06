89c96efbc6060101c288e98eb3d84b54d368ea15
isnou <nouioua0islem@gmail.com>
2023-06-06 13:59:06 +0100

    checked

--- src/main_manager/forms.py
+++ src/main_manager/forms.py
@@ -6,7 +6,7 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ('en_title', 'fr_title', 'ar_title', 'selected_image', 'brand', 'en_description', 'fr_description',
-                  'ar_description', 'en_note', 'fr_note', 'ar_note', 'price', 'discount')
+                  'ar_description', 'en_note', 'fr_note', 'ar_note', 'price', 'discount', 'is_activated')
 
 class VariantForm(ModelForm):
     class Meta:
