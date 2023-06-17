a7423a30d4b019b9ddc919bc75ef1dbddfae0a57
isnou <nouioua0islem@gmail.com>
2023-06-17 04:06:53 +0100

    dexunt

--- src/main_manager/admin.py
+++ src/main_manager/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Product, Variant, Option, Album, Feature
+from .models import Product, Variant, Option, Album, Feature, FlashProduct
+
+
 
 admin.site.register(Product)
 admin.site.register(Variant)
 admin.site.register(Option)
 admin.site.register(Album)
 admin.site.register(Feature)
+admin.site.register(FlashProduct)
