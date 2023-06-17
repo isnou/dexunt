ad1e42f1379e28a752a138d9e6378724dce612b4
isnou <nouioua0islem@gmail.com>
2023-06-17 04:23:30 +0100

    ss

--- src/main_manager/urls.py
+++ src/main_manager/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('<str:action>/statistics', views.admin_home, name='admin-home'),
     path('<str:action>/showcase', views.manage_showcase, name='manage-showcase'),
     path('<str:action>/products', views.manage_products, name='manage-products'),
+    path('<str:action>/flash', views.manage_flash, name='manage-flash'),
 ]
