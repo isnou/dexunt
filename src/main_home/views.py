5762ddba2aec7013141406c3078e03afb2c94db8
isnou <nouioua0islem@gmail.com>
2023-06-18 23:46:09 +0100

    {{ product.valid_until|time:'H' }}

--- src/main_home/views.py
+++ src/main_home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from add_ons import functions
 from authentication.forms import LoginForm, SignupForm
 from django.contrib.auth import login, authenticate
-from main_manager.models import Product, Variant, Option, Feature, Album
+from main_manager.models import Product, Variant, Option, Feature, Album, FlashProduct
 from main_manager.forms import ProductForm, VariantForm, FeatureForm, OptionForm
 from authentication.models import User
 
@@ -15,13 +15,16 @@ def home_page(request):
     signup_form = SignupForm()
 
     all_products = Product.objects.all()
+    all_flash_products = FlashProduct.objects.all()
 
     published_products = all_products.filter(is_activated=True)
+    published_flash_products = all_flash_products.exclude(is_activated=False).order_by('?')[:3]
 
     context = {
         'login_form': login_form,
         'signup_form': signup_form,
         'published_products': published_products,
+        'published_flash_products': published_flash_products,
     }
     return render(request, url, context)
 
