d29c4f31e0c7130500a2a12f2556e5b20c347c72
isnou <nouioua0islem@gmail.com>
2023-06-20 14:00:02 +0100

    selected_option.quantity += quantity - selected_product.quantity

--- src/main_manager/views.py
+++ src/main_manager/views.py
@@ -418,10 +418,14 @@ def manage_flash(request, action):
                 selected_product.quantity = selected_option.quantity
                 selected_option.quantity = 0
             else:
-                if quantity > selected_product.quantity:
-                    selected_option.quantity -= quantity - selected_product.quantity
+                selected_option.quantity += selected_product.quantity
+                selected_product.quantity = 0
+                if quantity > selected_option.quantity:
+                    selected_product.quantity = selected_option.quantity
+                    selected_option.quantity = 0
                 else:
-                    selected_option.quantity += selected_product.quantity - quantity
+                    selected_product.quantity = quantity
+                    selected_option.quantity -= quantity
                 selected_product.quantity = quantity
 
             selected_product.save()
