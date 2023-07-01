c8f8e6166680a5fa322d0d6a5b7eb08971b7d626
isnou <nouioua0islem@gmail.com>
2023-07-01 20:39:42 +0100

    perceverence

--- src/main_manager/views.py
+++ src/main_manager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from add_ons import functions
+from django.utils import timezone
 from authentication.forms import LoginForm, SignupForm
 from django.contrib.auth import login, authenticate
 from .models import Product, Variant, Option, Feature, Album, FlashProduct ,Description
