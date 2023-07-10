f86b0158bf8bccb145e58f5536a6de2476b73437
isnou <nouioua0islem@gmail.com>
2023-07-10 12:12:08 +0100

    Update urls.py

--- src/main_manager/urls.py
+++ src/main_manager/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<str:action>/statistics', views.admin_home, name='admin-home'),
-    path('<str:action>/showcase', views.manage_showcase, name='manage-showcase'),
     path('<str:action>/products', views.manage_products, name='manage-products'),
     path('<str:action>/flash', views.manage_flash, name='manage-flash'),
     path('<str:action>/orders', views.manage_orders, name='manage-orders'),
