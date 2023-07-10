6a462e0f55acd7bcd7d40101f71034a800645999
isnou <nouioua0islem@gmail.com>
2023-07-10 23:34:59 +0100

    signup_form

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -46,10 +46,9 @@ def account_login(request):
 def account_signup(request):
     if request.method == 'POST':
         signup_form = SignupForm(request.POST)
-        if signup_form.is_valid():
-            user = signup_form.save()
-            login(request, user)
-            return redirect('router')
+        user = signup_form.save()
+        login(request, user)
+        return redirect('router')
 
 @login_required
 def account_orders_page(request):
