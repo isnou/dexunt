2d753e6dd610530c428ad112411d094607d784db
isnou <nouioua0islem@gmail.com>
2023-07-11 20:12:32 +0100

    Nonsense

--- src/customer_manager/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<str:action>/statistics', views.customer_home, name='customer-home'),
-]
--- src/management/apps.py
+++ src/management/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainHomeConfig(AppConfig):
+class ManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main_home'
+    name = 'management'
