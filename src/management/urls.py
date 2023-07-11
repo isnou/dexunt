89d3d47f525a57a946b18d84b41d93fe8c394281
isnou <nouioua0islem@gmail.com>
2023-07-12 00:33:02 +0100

    dd

--- src/management/urls.py
+++ src/management/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:action>/statistics', views.admin_home, name='admin-home'),
-    path('<str:action>/products', views.manage_products, name='manage-products'),
-    path('<str:action>/flash', views.manage_flash, name='manage-flash'),
-    path('<str:action>/orders', views.manage_orders, name='manage-orders'),
-    path('<str:action>/shipping', views.manage_shipping, name='manage-shipping'),
-    path('<str:action>/coupon', views.manage_coupon, name='manage-coupon'),
+
+    path('<str:action>/statistics', views.admin_home, name='admin-manage-home'),
+    path('<str:action>/products', views.manage_products, name='admin-manage-products'),
+    path('<str:action>/flash', views.manage_flash, name='admin-manage-flash'),
+    path('<str:action>/orders', views.manage_orders, name='admin-manage-orders'),
+    path('<str:action>/shipping', views.manage_shipping, name='admin-manage-shipping'),
+    path('<str:action>/coupon', views.manage_coupon, name='admin-manage-coupon'),
 ]
