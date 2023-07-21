e5a897d10ca51ecb7f8a1de925069b2fc17bbd09
isnou <nouioua0islem@gmail.com>
2023-07-21 06:22:01 +0100

    xx

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -108,8 +108,7 @@ class User(AbstractUser):
             new_cart.save()
             self.cart = new_cart
         if not self.store:
-            new_store = Store(provider_token = self.token,
-                              en_name = self.username,
+            new_store = Store(en_name = self.username,
                               fr_name = self.username,
                               ar_name = self.username)
             new_store.save()
