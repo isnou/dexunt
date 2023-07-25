8703598a609ef938d65ae5e376ab1ca90ee0c086
isnou <nouioua0islem@gmail.com>
2023-07-25 18:18:46 +0100

    {% csrf_token %}

--- src/management/models.py
+++ src/management/models.py
@@ -305,10 +305,7 @@ class Store(models.Model):
 
         super().save()
     def activate(self):
-        if not self.user.profile_photo and not self.name:
-            self.is_activated = False
-        else:
-            self.is_activated = True
+        self.is_activated = True
         super().save()
 #                                                                        #
 class FlashProduct(models.Model):
