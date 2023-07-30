ca4524c775729504a1ad81d09a74e3d1662eee06
isnou <nouioua0islem@gmail.com>
2023-07-30 04:44:13 +0100

    default

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Wallet
+from .models import User, Wallet, DeliveryAddress
 
 admin.site.register(User)
 admin.site.register(Wallet)
+admin.site.register(DeliveryAddress)
 
