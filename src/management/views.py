0b31c3a95743aec6a674a8f295330361798f3a8e
isnou <nouioua0islem@gmail.com>
2023-08-02 00:14:51 +0100

    dd

--- src/management/views.py
+++ src/management/views.py
@@ -1261,7 +1261,7 @@ def provider_sales(request, action):
     if action == 'validate_order':
         order_id = request.GET.get('order_id', False)
         selected_order = request.user.store.orders.all().get(id=order_id)
-        selected_order.confirm()
+        selected_order.process()
         return redirect('provider-sales', 'main')
 
 
