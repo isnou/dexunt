53ca003f65685d7cb739d5f4379b53a4c760445d
isnou <nouioua0islem@gmail.com>
2023-08-01 22:06:13 +0100

    Update global_context.py

--- src/management/global_context.py
+++ src/management/global_context.py
@@ -1,9 +1,9 @@
 def orders(request):
     if request.user.is_authenticated:
-        new_orders_count = request.user.new_orders
+        new_orders_count = request.user.new_orders()
     else:
         new_orders_count = None
 
     return {
-        'new_orders_count': 0
+        'new_orders_count': new_orders_count
     }
