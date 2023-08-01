794d3a3a84c90278115f51f3524248bafe3f46bb
isnou <nouioua0islem@gmail.com>
2023-08-01 22:11:32 +0100

    new_orders_count

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -172,13 +172,13 @@ class User(AbstractUser):
         if self.is_superuser or self.is_admin or self.is_member:
             count = 0
             for o in Order.objects.all():
-                if o.status() == 'created':
+                if o.status == 'created':
                     count += 1
-            return 15
+            return count
         if self.is_provider:
             count = 0
             for o in self.store.orders.all():
-                if o.status() == 'confirmed':
+                if o.status == 'confirmed':
                     count += 1
             return count
     def client_name(self):
