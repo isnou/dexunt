97b10a6a1bcff4ac3910f04476ca0cdad9840672
isnou <nouioua0islem@gmail.com>
2023-08-24 13:04:23 +0100

    aa

--- src/home/models.py
+++ src/home/models.py
@@ -439,13 +439,13 @@ class Order(models.Model):
         if self.log.first():
             return self.log.first().at
     def tracking_log(self):
-        logs = self.log.all()
+        logs = self.log.all().exclude(content='collected')
         content='start'
         for l in self.log.all().exclude(content='collected'):
             if l.content == content:
                 logs.exclude(id=l.id)
             content = l.content
-        return logs.exclude(content='collected')
+        return logs
 #                                                                        #
 def get_order(request):
     selected_cart = get_cart(request)
