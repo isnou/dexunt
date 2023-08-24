394e203d4a175a120fe0883d043b2546fd354fe9
isnou <nouioua0islem@gmail.com>
2023-08-24 22:34:28 +0100

    ss

--- src/home/models.py
+++ src/home/models.py
@@ -435,7 +435,7 @@ class Order(models.Model):
             return (log.width()+47.9)/self.WIDTH
         if self.status == 'confirmed':
             log = self.log.all().get(content='confirmed')
-            return (log.width()+0)/self.WIDTH
+            return (log.width()+47.9)/self.WIDTH
         if self.status == 'processed':
             log = self.log.all().get(content='processed')
             return (log.width()+0)/self.WIDTH
