1fb1dfe4f24ef56db2d847a63cced27fa34e5c19
isnou <nouioua0islem@gmail.com>
2023-08-24 11:34:55 +0100

    nef

--- src/home/models.py
+++ src/home/models.py
@@ -445,7 +445,7 @@ class Order(models.Model):
             if l.content == content:
                 logs.exclude(id=l.id)
             content = l.content
-        return logs
+        return logs.exclude(content='collected')
 #                                                                        #
 def get_order(request):
     selected_cart = get_cart(request)
