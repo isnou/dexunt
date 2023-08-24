f8ed7bd5fdfeddc41efd08455e81d6a00e2f4022
isnou <nouioua0islem@gmail.com>
2023-08-24 09:41:13 +0100

    enorm

--- src/home/models.py
+++ src/home/models.py
@@ -439,7 +439,7 @@ class Order(models.Model):
         if self.log.first():
             return self.log.first().at
     def tracking_log(self):
-        return self.log.all().exclude(content='collected')
+        return self.log.all().exclude(content='collected').distinct('content', 'id')
 #                                                                        #
 def get_order(request):
     selected_cart = get_cart(request)
