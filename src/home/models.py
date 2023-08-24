8102ca49fb114b3187e027b80d9b41884b8b403f
isnou <nouioua0islem@gmail.com>
2023-08-24 13:15:34 +0100

    poly

--- src/home/models.py
+++ src/home/models.py
@@ -441,7 +441,7 @@ class Order(models.Model):
     def tracking_log(self):
         logs = self.log.all().exclude(content='collected')
         content='start'
-        for l in self.log.all().exclude(content='collected'):
+        for l in logs:
             if l.content == content:
                 logs = logs.exclude(id=l.id)
             content = l.content
