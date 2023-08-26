6c87e617caecc16a66ecf6e61006af7157bcce55
isnou <nouioua0islem@gmail.com>
2023-08-26 16:37:37 +0100

    floatformat:"2g"

--- src/management/views.py
+++ src/management/views.py
@@ -1221,7 +1221,7 @@ def member_orders(request, action):
     # --------------- main page ------------------- #
     if action == 'main':
         url = direction + "/management/member/orders/list.html"
-        all_orders = Order.objects.all().order_by('-updated_at')
+        all_orders = Order.objects.all().order_by('-id')
 
         if all_orders.count():
             paginate = True
