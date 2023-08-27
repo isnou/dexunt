89996a0f3d49c474baaa93a726aaa47eb0bcdc21
isnou <nouioua0islem@gmail.com>
2023-08-27 09:58:37 +0100

    jhonson

--- src/home/models.py
+++ src/home/models.py
@@ -391,6 +391,7 @@ class Order(models.Model):
                 new_status = None
         if new_status:
             self.status = new_status
+            super().save()
             self.new_log(request)
         else:
             super().save()
