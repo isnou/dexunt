d6771392cce87fb5bc6cabd2479c9b58350932ac
isnou <nouioua0islem@gmail.com>
2023-10-25 23:44:28 +0100

    ar_product_description_form

--- src/dexunt/settings.py
+++ src/dexunt/settings.py
@@ -151,8 +151,16 @@ CKEDITOR_CONFIGS = {
     'default': {
         # 'skin': 'minimalist',
         'skin': 'n1theme',
-        'toolbar_Basic': [
-            ['Source', '-', 'Bold', 'Italic']
+        'toolbar_Full': [
+            ['Styles', 'Format', 'Bold', 'Italic', 'Underline', 'Strike', 'SpellChecker', 'Undo', 'Redo'],
+            ['Link', 'Unlink', 'Anchor'],
+            ['Image', 'Flash', 'Table', 'HorizontalRule'],
+            ['TextColor', 'BGColor'],
+            ['Smiley', 'SpecialChar'], ['Source'],
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+            ['NumberedList','BulletedList'],
+            ['Indent','Outdent'],
+            ['Maximize'],
         ],
         'toolbar_YourCustomToolbarConfig': [
             {'name': 'document', 'items': ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates']},
