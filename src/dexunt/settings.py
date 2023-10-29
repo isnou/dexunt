56ae3b733e01c9d007738fd8f636dad2c163663b
isnou <nouioua0islem@gmail.com>
2023-10-29 13:09:19 +0100

    dds

--- src/dexunt/settings.py
+++ src/dexunt/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'home.global_context.language',
+                'home.global_context.session_language',
             ],
         },
     },
