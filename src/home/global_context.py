955a871702064f290c00bcecf5db1751b778c7a8
isnou <nouioua0islem@gmail.com>
2023-10-29 12:50:32 +0100

    ss

--- src/home/global_context.py
+++ src/home/global_context.py
@@ -1,8 +1,24 @@
 from add_ons.functions import text_selector
 
 def language(request):
+    if not request.session.get('language', None):
+        request.session['language'] = 'en-us'
+    selected_language = request.session.get('language')
+    en_us = False
+    fr_fr = False
+    ar_dz = False
+
+    if selected_language == 'en-us':
+        en_us = True
+    if selected_language == 'fr-fr':
+        fr_fr = True
+    if selected_language == 'ar-dz':
+        ar_dz = True
+
 
     return {
-        'txt_track_my_order':text_selector('track my order', 'suivre ma commande', 'تابع طلبي', request),
-        'language': request.session.get('language'),
+        'txt_track_my_order':text_selector(selected_language, 'track my order', 'suivre ma commande', 'تابع طلبي'),
+        'en_us': en_us,
+        'fr_fr': fr_fr,
+        'ar_dz': ar_dz,
     }
