186f90a82da0696d7acf95e84769e70e99a20632
isnou <nouioua0islem@gmail.com>
2023-11-04 16:06:36 +0100

    ecx</form>

--- src/management/models.py
+++ src/management/models.py
@@ -388,7 +388,7 @@ class Product(models.Model):
     def unselected_tags(self):
         selected_tags = Tag.objects.all()
         for o_t in self.tags.all():
-            selected_tags.exclude(id=o_t.id)
+            selected_tags.exclude(id=1)
         return selected_tags
 # ---------------------------------------------------------------------- #
 
