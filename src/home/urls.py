eabbb634fc4214839424e3df6545ab44e5b9a60a
isnou <nouioua0islem@gmail.com>
2023-11-10 07:07:54 +0100

    {% url 'shop-page' 'grid' %}

--- src/home/urls.py
+++ src/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home-page'),
     path('product/<str:action>', views.product_page, name='product-page'),
+    path('shop/<str:action>', views.shop_page, name='shop-page'),
     path('shopping/cart/<str:action>', views.shopping_cart_page, name='shopping-cart'),
     path('order/page/<str:action>', views.order_page, name='order-page'),
     path('change/language', views.change_language, name='change-language'),
