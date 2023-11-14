58544e77cbe91298b8f0a896567b6bd8cc883399
isnou <nouioua0islem@gmail.com>
2023-11-15 00:40:48 +0100

    ssreturn self.ref

--- src/home/models.py
+++ src/home/models.py
@@ -228,8 +228,11 @@ class Cart(models.Model):
         'home.Coupon', on_delete=models.CASCADE, blank=True, null=True)
     # ----- functions ----- #
     def __str__(self):
-        if self.user is None:
+        if self.user:
+            return self.user.username
+        else:
             return self.ref
+
 
     def save(self, *args, **kwargs):
         if not self.ref:
