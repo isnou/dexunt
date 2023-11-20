6f03da9476b00755d86bf8b549b53d495e0e432c
isnou <nouioua0islem@gmail.com>
2023-11-20 01:49:12 +0100

    sss

--- src/management/models.py
+++ src/management/models.py
@@ -132,11 +132,12 @@ class Option(models.Model):
     def save(self, *args, **kwargs):
         if not self.upc:
             self.upc = functions.serial_number_generator(20).upper()
-        if self.limited_stock():
-            if self.production_capacity_quantity:
-                self.out_of_stock = False
-            else:
-                self.out_of_stock = True
+        else:
+            if self.limited_stock():
+                if self.production_capacity_quantity:
+                    self.out_of_stock = False
+                else:
+                    self.out_of_stock = True
         super().save()
     def duplicate(self):
         new_option = Option(delivery_quotient = self.delivery_quotient,
