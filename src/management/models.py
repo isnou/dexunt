c5baa6455f14e80c607175009fb179b00c1444d0
isnou <nouioua0islem@gmail.com>
2023-11-23 18:05:47 +0100

    ddd

--- src/management/models.py
+++ src/management/models.py
@@ -499,7 +499,7 @@ class Product(models.Model):
         collection = Collection.objects.all().get(id=co_id)
         collection.product.remove(self)
     def tags_form_text(self, text):
-        for t in text.split():
+        for t in text.len():
             if t.count() < 4:
                 if Tag.objects.all().filter(title=t).exists():
                     if not self.tags.filter(title=t).exists():
