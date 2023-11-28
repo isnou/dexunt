a8a4020b68f17e0efa3fc71d7289fabbc92dcad9
isnou <nouioua0islem@gmail.com>
2023-11-29 00:31:58 +0100

    ss

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import User, Wallet, Transaction, DeliveryAddress
+from .models import User
 
 admin.site.register(User)
-admin.site.register(Wallet)
-admin.site.register(Transaction)
-admin.site.register(DeliveryAddress)
 
