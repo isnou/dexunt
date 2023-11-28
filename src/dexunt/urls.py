c9538451854c30fc96b50d721be6538c25c995ab
isnou <nouioua0islem@gmail.com>
2023-11-29 00:35:20 +0100

    ss

--- src/dexunt/urls.py
+++ src/dexunt/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('globals/', include('management.urls')),
     path('users/', include('authentication.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
