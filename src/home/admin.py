ddeacabb87425a82339ea7f27c73f98ace2b0bfd
isnou <nouioua0islem@gmail.com>
2023-11-29 00:31:19 +0100

    ss

--- src/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cart ,SelectedProduct ,Coupon ,Order ,Municipality ,Province
-
-admin.site.register(Cart)
-admin.site.register(SelectedProduct)
-admin.site.register(Province)
-admin.site.register(Municipality)
-admin.site.register(Order)
