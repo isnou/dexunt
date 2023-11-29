98440cb6038fe5bb26867aec773370ab7b01869f
isnou <nouioua0islem@gmail.com>
2023-11-29 23:46:00 +0100

    ss

--- src/home/views.py
+++ src/home/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-#from add_ons.functions import serial_number_generator
-#from add_ons.variables import get_cart, categories
+from globals.functions import text_selector, session_manager
 from authentication.forms import LoginForm, SignupForm
 from django.contrib.auth import login, authenticate
 from authentication.models import User
 
 
 def home_page(request):
+    session_manager()
     direction = request.session.get('direction')
 
     url = direction + "/home/main.html"
