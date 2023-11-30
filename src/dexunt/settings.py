ac6e0d249e6e1a3dbcad7fe33776d0ad9c0bacd9
isnou <nouioua0islem@gmail.com>
2023-11-30 02:05:04 +0100

    ss

--- src/dexunt/settings.py
+++ src/dexunt/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'globals.context.manager',
+                'globals.context.top_bar',
             ],
         },
     },
--- src/globals/context.py
+++ src/globals/context.py
@@ -4,14 +4,17 @@ def manager(request):
     session_manager(language = request.session.get('language'))
 
     return {
-        'test': text_selector(
-            'Track my order',
-            'Suivre ma commande',
-            'تابع طلبي'
-        ),
-
         # --------------------------  language -------------------------- #
         'selected_language': request.session.get('selected_language', None),
         'second_language': request.session.get('second_language', None),
         'third_language': request.session.get('third_language', None),
+    }
+
+def top_bar():
+    return {
+        'login': text_selector(
+            'login',
+            'se connecter',
+            'تسجيل'
+        ),
     }
