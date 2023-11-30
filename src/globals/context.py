8fa65a3edab91e19464c14229f407f6458e8ccc6
isnou <nouioua0islem@gmail.com>
2023-11-30 02:06:38 +0100

    ss

--- src/globals/context.py
+++ src/globals/context.py
@@ -4,17 +4,14 @@ def manager(request):
     session_manager(language = request.session.get('language'))
 
     return {
-        # --------------------------  language -------------------------- #
-        'selected_language': request.session.get('selected_language', None),
-        'second_language': request.session.get('second_language', None),
-        'third_language': request.session.get('third_language', None),
-    }
-
-def top_bar():
-    return {
+        # ---------------------------  topbar --------------------------- #
         'login': text_selector(
             'login',
             'se connecter',
             'تسجيل'
         ),
+        # --------------------------  language -------------------------- #
+        'selected_language': request.session.get('selected_language', None),
+        'second_language': request.session.get('second_language', None),
+        'third_language': request.session.get('third_language', None),
     }
