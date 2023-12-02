d12616543a0aaa1f5cfd051625b571b6837f0d5a
isnou <nouioua0islem@gmail.com>
2023-12-02 03:57:01 +0100

    dds

--- src/home/urls.py
+++ src/home/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.home_page, name='home-page'),
+    
     path('change/language', views.change_language, name='change-language'),
+    path('router/', views.router, name='router'),
 ]
